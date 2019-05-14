ba694ac4f47c4f1eade203ea33c3ca0b2c41fa6a
stipan <micromillioner@yahoo.com>
2019-05-14 08:22:09 +0200

    Added example of Euclidian Distance for images.

--- modules/distance/examples/EMD_example.cpp
+++ modules/distance/examples/EMD_example.cpp
@@ -35,23 +35,14 @@ void vector_print(const std::vector<T> &vec)
 
 int main()
 {
-	//-----------------------------------------------
-	// The ground distance - thresholded Euclidean distance.
-	// Since everything is ints, we multiply by COST_MULT_FACTOR.
+	/******************** examples for Euclidean Distance **************************/
+	// example for picture
+
 
 	typedef int edm_Type;
 
-	// const edm_Type COST_MULT_FACTOR = 1000;
-	// const edm_Type THRESHOLD = 3 * COST_MULT_FACTOR; //1.412*COST_MULT_FACTOR;
-	//                                                  // std::vector< std::vector<int> > cost_mat; // here it's defined as global for Rubner's interfaces.
-	//                                                  // If you do not use Rubner's interface it's a better idea
-	//                                                  // not to use globals.
-	//                                                  // unsigned int im1_R = i0.size();
-	//                                                  // unsigned int im1_C = i0.size();
-
 	size_t im1_R = img1.size() / 6;
 	size_t im1_C = img1[0].size() / 6;
-	size_t n = im1_R * im1_C;
 
 	// serialize_mat2vec
 	std::vector<edm_Type> i1;
@@ -65,44 +56,33 @@ int main()
 		{
 			i1.push_back(img1[i][j]);
 			i2.push_back(img2[i][j]);
-			i3.push_back(img3[i][j]);
-			i4.push_back(img4[i][j]);
 		}
 	}
 
 	auto cost_mat = metric::distance::EMD_details::ground_distance_matrix_of_2dgrid<edm_Type>(im1_C, im1_R);
 	auto maxCost = metric::distance::EMD_details::max_in_distance_matrix(cost_mat);
 
-	std::cout << "bis hier" << std::endl;
+	std::cout << "we have started" << std::endl;
+	std::cout << "" << std::endl;
 
 	//matrix_print(cost_mat);
 
-	metric::distance::EMD<edm_Type> distance2(cost_mat, maxCost);
+	metric::distance::EMD<edm_Type> distance(cost_mat, maxCost);
 
-	//auto result1 = distance(v0, v2);
-
+	// assumes that i1 and i2 are serialized vectors of the image matrices, and cost_mat contains a distance matrix that takes into account the original pixel locations.
 	auto t1 = std::chrono::steady_clock::now();
-
-	// geht davon aus, dass i1 und i2 serialisierte Vectoren der Bildmatrizen sind und cost_mat eine passende Distanzematrix enthält, die jedoch die orgiginalen Pixelpositionen berücksichtigt.
-	auto result1 = distance2(i1, i2);
+	auto result1 = distance(i1, i2);
 	auto t2 = std::chrono::steady_clock::now();
-	std::cout << "-------r" << std::endl;
-	auto result2 = distance2(i2, i1);
+	std::cout << "result1: " << result1 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000 << " s)" << std::endl;
+	std::cout << "" << std::endl;
+
+	std::cout << "swap records and calculate again" << std::endl;
+	std::cout << "" << std::endl;
+
+	auto result2 = distance(i2, i1);
 	auto t3 = std::chrono::steady_clock::now();
-
-	std::cout << "result1: " << result1 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000 << "s)" << std::endl;
-	std::cout << "result2: " << result2 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count()) / 1000000 << "s)" << std::endl;
-
-
-	/******************** examples for Euclidean Distance **************************/
-	// example for number
-	// example for string
-	// example for date
-	// example for array
-	// example for curve
-	// example for sentence
-	// example for soundwave
-	// example for picture
-
+	std::cout << "result2: " << result2 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count()) / 1000000 << " s)" << std::endl;
+	std::cout << "" << std::endl;
+	
 	return 0;
 }
