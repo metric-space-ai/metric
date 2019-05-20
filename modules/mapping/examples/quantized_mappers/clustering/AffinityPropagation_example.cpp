5838b1c278243ce4855d1a968cb00cbf96f40977
stipan <micromillioner@yahoo.com>
2019-05-20 11:38:24 +0200

    Added Affinity Propagation example

--- modules/mapping/examples/quantized_mappers/clustering/AffinityPropagation_example.cpp
+++ modules/mapping/examples/quantized_mappers/clustering/AffinityPropagation_example.cpp
@@ -1,13 +1,4 @@
 
-#include "../details/classification/metric_classification.hpp"
-
-#include "assets/helpers.cpp" // csv reader
-
-#include <variant>
-
-#include <deque> // for Record test
-
-#include "../details/classification/details/correlation_weighted_accuracy.hpp"
 #include "../details/classification/details/3rdparty/metric_clustering/clustering.hpp"
 
 
@@ -25,7 +16,7 @@ int main()
 		   {5.81414000000000, 8.14015000000000, 3.22950000000000, 139.539000000000, 139.539000000000},
 		   {2.57927000000000, 2.63399000000000, 2.46802000000000, 61.9026000000000, 61.9026000000000} };
 
-	auto[assignments, seeds, counts] = clustering::dbscan(data, (float) 64.0, 1); 
+	auto[assignments, exemplars, counts] = clustering::affprop(data);
 
 
 	std::cout << "assignments:" << std::endl;
@@ -42,16 +33,16 @@ int main()
 	}
 	std::cout << '\n';
 
-	std::cout << "seeds:" << std::endl;
-	for (size_t i = 0; i < seeds.size(); i++)
+	std::cout << "exemplars:" << std::endl;
+	for (size_t i = 0; i < exemplars.size(); i++)
 	{
-		if (i < seeds.size() - 1)
+		if (i < exemplars.size() - 1)
 		{
-			std::cout << seeds[i] << ", ";
+			std::cout << exemplars[i] << ", ";
 		}
 		else
 		{
-			std::cout << seeds[i] << std::endl;
+			std::cout << exemplars[i] << std::endl;
 		}
 	}
 	std::cout << '\n';
