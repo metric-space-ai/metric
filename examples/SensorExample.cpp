f405bb9b92ee17d76283b8f084da3903eef84f73
stipan <micromillioner@yahoo.com>
2019-07-08 15:41:09 +0200

    lilfix

--- examples/SensorExample.cpp
+++ examples/SensorExample.cpp
@@ -866,22 +866,25 @@ int main(int argc, char *argv[])
 
 
 	auto e = entropy<double, metric::distance::P_norm<double>>(dataset_0_i, 3, 2, metric::distance::P_norm<double>(3));
-	std::cout << "H(X,Y) General Minkowsky, 3: " << e << std::endl;
+	std::cout << "H(X) General Minkowsky, 3: " << e << std::endl;
 
 	e = entropy<double, metric::distance::P_norm<double>>(dataset_0_i, 3, 2, metric::distance::P_norm<double>(2));
-	std::cout << "H(X,Y) General Minkowsky, 2: " << e << std::endl;
+	std::cout << "H(X) General Minkowsky, 2: " << e << std::endl;
 
 	e = entropy<double, metric::distance::Euclidian<double>>(dataset_0_i, 3, 2, metric::distance::Euclidian<double>());
-	std::cout << "H(X,Y) Euclidean: " << e << std::endl;
+	std::cout << "H(X) Euclidean: " << e << std::endl;
 
 	e = entropy<double, metric::distance::P_norm<double>>(dataset_0_i, 3, 2, metric::distance::P_norm<double>(1));
-	std::cout << "H(X,Y) General Minkowsky, 1: " << e << std::endl;
+	std::cout << "H(X) General Minkowsky, 1: " << e << std::endl;
 
 	e = entropy<double, metric::distance::Manhatten<double>>(dataset_0_i, 3, 2, metric::distance::Manhatten<double>());
-	std::cout << "H(X,Y) Manhatten: " << e << std::endl;
+	std::cout << "H(X) Manhatten: " << e << std::endl;
 
 	e = entropy<double, metric::distance::Chebyshev<double>>(dataset_0_i, 3, 2, metric::distance::Chebyshev<double>());
-	std::cout << "H(X,Y) Chebyshev: " << e << std::endl;
+	std::cout << "H(X) Chebyshev: " << e << std::endl;
+
+	e = entropy<double, metric::distance::Chebyshev<double>>(dataset_1_i, 3, 2, metric::distance::Chebyshev<double>());
+	std::cout << "H(Y) Chebyshev: " << e << std::endl;
 
 
 	std::cout << "\n";
