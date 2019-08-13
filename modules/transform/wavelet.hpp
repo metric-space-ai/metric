64016345b0a5764b9207adabe38979a7a0101d8e
Dmitry Vinokurov <dmitry.vinokurov@gmail.com>
2019-08-14 00:04:32 +0600

    format modules/transform

--- modules/transform/wavelet.hpp
+++ modules/transform/wavelet.hpp
@@ -1,6 +1,3 @@
-#ifndef METRIC_WAVLET_H
-#define METRIC_WAVLET_H
-
 /*
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,92 +6,167 @@
   Copyright (c) 2019 Michael Welsch
 */
 
+#ifndef _METRIC_TRANSFORM_WAVLET_HPP
+#define _METRIC_TRANSOFRM_WAVLET_HPP
+
 #include <iostream>
-//#include <stdexcept>
 #include <tuple>
 #include <cmath>
 #include <vector>
-//#include <numeric>
 #include <algorithm>
 #include <functional>
 #include <deque>
 #include "../../3rdparty/blaze/Math.h"
 
-
 namespace wavelet {
 
-// valid convolution
+/**
+ * @brief valid convolution
+ * 
+ * @param f 
+ * @param g 
+ * @return 
+ */
 template <typename T>
-std::vector<T>
-conv_valid(std::vector<T> const &f, std::vector<T> const &g);
+std::vector<T> conv_valid(std::vector<T> const& f, std::vector<T> const& g);
 
-// full convolution
+/**
+ * @brief full convolution
+ * 
+ * @param f 
+ * @param g 
+ * @return 
+ */
 template <typename T>
-std::vector<T>
-conv(std::vector<T> const &f, std::vector<T> const &g);
+std::vector<T> conv(std::vector<T> const& f, std::vector<T> const& g);
 
-// linspace (erzeugt einen linearen Datenvektor)
-template<typename T>
-std::vector<T>
-linspace(T a, T b, int n);
-
-// upsconv
+/**
+ * @brief linspace (erzeugt einen linearen Datenvektor)
+ * 
+ * @param a 
+ * @param b 
+ * @param n 
+ * @return
+ */
 template <typename T>
-std::vector<T>
-upsconv(std::vector<T> const &x, std::vector<T> const &f, int len);
+std::vector<T> linspace(T a, T b, int n);
 
-// dbwavf
-template<typename T>
-std::vector<T>
-dbwavf(int const wnum, T dings);
-
-// orthfilt
-template<typename T>
-std::tuple<std::vector<T>, std::vector<T>, std::vector<T>, std::vector<T>>
-orthfilt(std::vector<T> const &W_in);
-
-// dwt
-template<typename T>
-std::tuple<std::vector<T>, std::vector<T>>
-dwt(std::vector<T> const &x, int waveletType);
-
-// idwt
-template<typename T>
-std::vector<T>
-idwt(std::vector<T> a, std::vector<T> d, int waveletType, int lx);
-
-// wmaxlev
-int 
-wmaxlev(int sizeX, int waveletType);
-
-// wavedec;
-template<typename T>
-std::deque<std::vector<T>>
-wavedec(std::vector<T> const &x, int order, int waveletType);
-
-// waverec
-template<typename T>
-std::vector<T>
-waverec(std::deque<std::vector<T>> const &subBands, int waveletType);
-
-//denoise
-template<typename T>
-std::deque<std::vector<T>>
-denoise(std::deque<std::vector<T>> const &data, T const &tresh);
-
-// sparse
-template<typename T>
-std::tuple<std::deque<std::vector<T>>, std::deque<std::vector<T>>>
-sparse(std::deque<std::vector<T>> const &data);
-
-// distance measure by time elastic cost matrix.
+/**
+ * @brief upsconv
+ * 
+ * @param x 
+ * @param f 
+ * @param len 
+ * @return
+ */
 template <typename T>
-T TWED(blaze::CompressedVector<T> const &As, blaze::CompressedVector<T> const &Bs, T const &penalty = 0, T const &elastic = 1);
+std::vector<T> upsconv(std::vector<T> const& x, std::vector<T> const& f, int len);
 
+/**
+ * @brief 
+ * 
+ * @param wnum 
+ * @param dings 
+ * @return
+ */
+template <typename T>
+std::vector<T> dbwavf(int const wnum, T dings);
 
+/**
+ * @brief 
+ * 
+ * @param W_in 
+ * @return
+ */
+template <typename T>
+std::tuple<std::vector<T>, std::vector<T>, std::vector<T>, std::vector<T>> orthfilt(std::vector<T> const& W_in);
 
-} // namespace
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param waveletType 
+ * @return
+ */
+template <typename T>
+std::tuple<std::vector<T>, std::vector<T>> dwt(std::vector<T> const& x, int waveletType);
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param d 
+ * @param waveletType 
+ * @param lx 
+ * @return
+ */
+template <typename T>
+std::vector<T> idwt(std::vector<T> a, std::vector<T> d, int waveletType, int lx);
+
+/**
+ * @brief 
+ * 
+ * @param sizeX 
+ * @param waveletType 
+ * @return
+ */
+int wmaxlev(int sizeX, int waveletType);
+
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param order 
+ * @param waveletType 
+ * @return
+ */
+template <typename T>
+std::deque<std::vector<T>> wavedec(std::vector<T> const& x, int order, int waveletType);
+
+/**
+ * @brief 
+ * 
+ * @param subBands 
+ * @param waveletType 
+ * @return
+ */
+template <typename T>
+std::vector<T> waverec(std::deque<std::vector<T>> const& subBands, int waveletType);
+
+/**
+ * @brief 
+ * 
+ * @param data 
+ * @param tresh 
+ * @return
+ */
+template <typename T>
+std::deque<std::vector<T>> denoise(std::deque<std::vector<T>> const& data, T const& tresh);
+
+/**
+ * @brief 
+ * 
+ * @param data 
+ * @return
+ */
+template <typename T>
+std::tuple<std::deque<std::vector<T>>, std::deque<std::vector<T>>> sparse(std::deque<std::vector<T>> const& data);
+
+/**
+ * @brief distance measure by time elastic cost matrix.
+ * 
+ * @param As 
+ * @param Bs 
+ * @param penalty 
+ * @param elastic 
+ * @return 
+ */
+template <typename T>
+T TWED(blaze::CompressedVector<T> const& As, blaze::CompressedVector<T> const& Bs, T const& penalty = 0,
+    T const& elastic = 1);
+
+}  // namespace
 
 #include "wavelet.cpp"
 
-#endif // METRIC_WAVLET_H
+#endif
