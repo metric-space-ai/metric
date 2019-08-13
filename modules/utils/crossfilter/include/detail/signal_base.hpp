d7e227f9c73c012a597d0a699a5dc88c87b753a2
Dmitry Vinokurov <dmitry.vinokurov@gmail.com>
2019-08-13 23:36:58 +0600

    move utils directory to modules

---
+++ modules/utils/crossfilter/include/detail/signal_base.hpp
@@ -0,0 +1,115 @@
+/*This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2018 Dmitry Vinokurov */
+
+#ifndef _METRIC_CROSSFILTER_DETAILS_SIGNAL_BASE_HPP
+#define _METRIC_CROSSFILTER_DETAILS_SIGNAL_BASE_HPP
+#define USE_NOD_SIGNALS = 1
+#include <tuple>
+#include <memory>
+#include "../detail/thread_policy.hpp"
+
+#if defined USE_NOD_SIGNALS
+#include "../3dparty/nod.hpp"
+
+#ifdef CROSS_FILTER_USE_THREAD_POOL
+#define BOOST_ERROR_CODE_HEADER_ONLY
+#include <boost/asio.hpp>
+#endif
+
+namespace cross {
+namespace signals {
+    template <typename F>
+    using signal = nod::signal<F>;
+    using connection = nod::connection;
+}
+#else
+#include <boost/signals2.hpp>
+namespace signals {
+template <typename F>
+using signal = boost::signals2::signal<F>;
+using connection = boost::signals2::connection;
+}
+#endif
+namespace impl {
+    template <class F, class Tuple, std::size_t... I>
+    constexpr decltype(auto) apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
+    {
+        return f(std::get<I>(std::forward<Tuple>(t))...);
+    }
+    template <class F, class Tuple>
+    constexpr decltype(auto) apply(F&& f, Tuple&& t)
+    {
+        return apply_impl(std::forward<F>(f), std::forward<Tuple>(t),
+            std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>::value> {});
+    }
+
+    template <typename Signal, typename Connection>
+    struct MovableSignal {
+
+        std::unique_ptr<Signal> signal;
+        //  std::mutex mutex;
+        MovableSignal(const MovableSignal&) = delete;
+        MovableSignal& operator=(const MovableSignal&) = delete;
+        MovableSignal()
+            : signal(std::make_unique<Signal>())
+        {
+        }
+
+        MovableSignal(MovableSignal&& s) noexcept
+            : signal(std::move(s.signal))
+        {
+        }
+
+        MovableSignal& operator=(MovableSignal&& s) noexcept
+        {
+            if (&s == this)
+                return *this;
+            std::swap(signal, s.signal);
+            return *this;
+        }
+
+        template <typename F>
+        Connection connect(const F& f)
+        {
+            //    std::lock_guard<std::mutex> lk(mutex);
+            return signal->connect(f);
+        }
+        template <typename... Args>
+        void operator()(Args&&... args)
+        {
+            if (signal)
+                signal->operator()(args...);
+        }
+#ifdef CROSS_FILTER_USE_THREAD_POOL
+        template <typename... Args>
+        void emit_in_pool(uint32_t thread_pool_size, Args&&... args)
+        {
+            if (thread_pool_size == 0) {
+                this->operator()(args...);
+            } else {
+                if (signal) {
+                    boost::asio::thread_pool pool(thread_pool_size);
+                    auto tpl = std::make_tuple(std::forward<Args>(args)...);
+                    signal->emit_in_pool(
+                        [&pool, &tpl](auto& slot) { boost::asio::post(pool, [&slot, &tpl] { apply(slot, tpl); }); });
+                    pool.join();
+                }
+            }
+        }
+#endif
+        int num_slots()
+        {
+            //    std::lock_guard<std::mutex> lk(mutex);
+#if defined USE_NOD_SIGNALS
+            return signal->slot_count();
+#else
+        return signal->num_slots();
+#endif
+        }
+    };
+}  // namespace impl
+}  // namespace cross
+#endif
---
+++ modules/utils/graph/sparsify.h
@@ -0,0 +1,55 @@
+/*
+		This Source Code Form is subject to the terms of the Mozilla Public
+		License, v. 2.0. If a copy of the MPL was not distributed with this
+		file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+		Copyright (c) 2019 Panda Team
+		Copyright (c) 2019 Oleg Popov
+*/
+/*
+Laplacians is a package containing graph algorithms, with an emphasis on tasks related to 
+spectral and algebraic graph theory. It contains (and will contain more) code for solving 
+systems of linear equations in graph Laplacians, low stretch spanning trees, sparsifiation, 
+clustering, local clustering, and optimization on graphs.
+
+All graphs are represented by sparse adjacency matrices. This is both for speed, and because 
+our main concerns are algebraic tasks. It does not handle dynamic graphs. It would be very slow 
+to implement dynamic graphs this way.
+
+https://github.com/danspielman/Laplacians.jl
+*/
+
+#pragma once
+
+#include <cmath>
+#include "../../../3rdparty/blaze/Math.h"
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include "../../../utils/solver/solvertypes.h"
+#include "../../../utils/solver/approxchol.h"
+#include "../../../utils/solver/graphalgs.h"
+//#include "crandom.h"
+
+using namespace std;
+using blaze::CompressedMatrix;
+using blaze::DynamicMatrix;
+using blaze::DynamicVector;
+
+/*
+	Just implements Spielman-Srivastava
+		as = sparsify(a; ep=0.5)
+
+	Apply Spielman-Srivastava sparsification: sampling by effective resistances.
+	`ep` should be less than 1.
+*/
+
+
+namespace laplacians {
+
+	template<typename Tv>
+	CompressedMatrix<Tv, blaze::columnMajor> sparsify(const CompressedMatrix<Tv, blaze::columnMajor>& a,
+		float ep = 0.3F, float matrixConcConst = 4.0F, float JLfac = 4.0F);
+}
+
+#include "sparsify.cpp"
