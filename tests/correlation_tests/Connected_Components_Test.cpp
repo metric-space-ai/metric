54e70134e86d5e8addcbaae2142c9d7ac1f375fa
Dmitry Vinokurov <dmitry.vinokurov@gmail.com>
2019-08-13 23:38:05 +0600

    reorganize correlation module

--- tests/correlation_tests/Connected_Components_Test.cpp
+++ tests/correlation_tests/Connected_Components_Test.cpp
@@ -10,145 +10,129 @@
 #include <string>
 #include "../details/connected-components.hpp"
 
-
-
 template <typename T>
 class Matrix {
-	std::vector<std::vector<T>> mx;
+    std::vector<std::vector<T>> mx;
 
 public:
-	using ElementType = T;
-	Matrix() {}
+    using ElementType = T;
+    Matrix() {}
 
-	Matrix(size_t size) : mx(size, std::vector<T>(size)){
-	}
-	
-	Matrix(size_t size, size_t, bool): mx(size, std::vector<T>(size)) {}
+    Matrix(size_t size)
+        : mx(size, std::vector<T>(size))
+    {
+    }
 
-	 typedef T value_type;
+    Matrix(size_t size, size_t, bool)
+        : mx(size, std::vector<T>(size))
+    {
+    }
 
-	size_t size() const {
-		return mx.size();
-	}
-	void resize(size_t new_size, size_t) {
-	    mx.resize(new_size);
-	    for(auto & v : mx) {
-		v.resize(new_size);
-	    }
-	}
-	size_t rows() const {
-	    return mx.size();
-	}
-	std::vector<T>& operator[](const size_t row) {
-		return mx[row];
-	}
+    typedef T value_type;
 
-	std::vector<T> operator[](const size_t row) const{
-		return mx[row];
-	}
-	
-	T & operator()(size_t row, size_t col)  {
-	    return mx[row][col];
-	}
-	
-	T operator()(size_t row, size_t col) const {
-	    return mx[row][col];
-	}
-	
+    size_t size() const { return mx.size(); }
+    void resize(size_t new_size, size_t)
+    {
+        mx.resize(new_size);
+        for (auto& v : mx) {
+            v.resize(new_size);
+        }
+    }
+    size_t rows() const { return mx.size(); }
+    std::vector<T>& operator[](const size_t row) { return mx[row]; }
 
+    std::vector<T> operator[](const size_t row) const { return mx[row]; }
 
-	void reset() {
-		for (auto& e : mx)
-			e.assign(size(), 0);
-	}
+    T& operator()(size_t row, size_t col) { return mx[row][col]; }
 
-	void resize(size_t size) {
-		std::vector<T> v(size);
-		mx.resize(size, v);
-	}
+    T operator()(size_t row, size_t col) const { return mx[row][col]; }
+
+    void reset()
+    {
+        for (auto& e : mx)
+            e.assign(size(), 0);
+    }
+
+    void resize(size_t size)
+    {
+        std::vector<T> v(size);
+        mx.resize(size, v);
+    }
 };
-template<typename Ty>
-void MatrixTest(Matrix<Ty> M) {
-	for (size_t i = 0; i < M.size(); ++i) {
-		for (size_t j = 0; j < M.size(); ++j)
-			if (M[i][j])
-				std::cout << i << "," << j << " ";
-			else
-				std::cout << "    ";
+template <typename Ty>
+void MatrixTest(Matrix<Ty> M)
+{
+    for (size_t i = 0; i < M.size(); ++i) {
+        for (size_t j = 0; j < M.size(); ++j)
+            if (M[i][j])
+                std::cout << i << "," << j << " ";
+            else
+                std::cout << "    ";
 
-		std::cout << std::endl;
-	}
+        std::cout << std::endl;
+    }
 
-	std::cout << std::endl << "===========================================" << std::endl;
+    std::cout << std::endl << "===========================================" << std::endl;
 }
-
-
-
 
 int main()
 {
-	std::vector<Matrix<unsigned>> res;
+    std::vector<Matrix<unsigned>> res;
 
-	Matrix<unsigned> M(9);
+    Matrix<unsigned> M(9);
 
-	std::cout << "Empty graph (with 0 vertices):" << std::endl;
+    std::cout << "Empty graph (with 0 vertices):" << std::endl;
 
-	res = connected_components(M, 0);
+    res = connected_components(M, 0);
 
-	for (auto mx : res)
-		MatrixTest(mx);
+    for (auto mx : res)
+        MatrixTest(mx);
 
-	M[0][1] = M[1][0] = M[1][2] = M[2][1] = M[2][4] = M[4][2] = M[4][3] = M[3][4] = M[2][5] = M[5][2] =
-		M[3][6] = M[6][3] = M[7][3] = M[3][7] = M[7][8] = M[8][7] = 
-		M[5][8] = M[8][5] = true;
-	
-	res= connected_components(M, 0);
+    M[0][1] = M[1][0] = M[1][2] = M[2][1] = M[2][4] = M[4][2] = M[4][3] = M[3][4] = M[2][5] = M[5][2] = M[3][6]
+        = M[6][3] = M[7][3] = M[3][7] = M[7][8] = M[8][7] = M[5][8] = M[8][5] = true;
 
-	std::cout << "Full graph:" << std::endl;
+    res = connected_components(M, 0);
 
-	for (auto mx : res)
-		MatrixTest(mx); 
-			
+    std::cout << "Full graph:" << std::endl;
 
-	std::cout << std::endl << std::endl;
-	
-	//We will disconnect (2, 4) and (5, 8) pairs of vertices
-	//the graph will fall to two components
+    for (auto mx : res)
+        MatrixTest(mx);
 
-	M[4][2] = M[2][4] = M[5][8] = M[8][5] = false;
+    std::cout << std::endl << std::endl;
 
-	res = connected_components(M,0);
+    //We will disconnect (2, 4) and (5, 8) pairs of vertices
+    //the graph will fall to two components
 
-	std::cout << "Splitted graph:" << std::endl;
+    M[4][2] = M[2][4] = M[5][8] = M[8][5] = false;
 
-	for (auto mx : res)
-		MatrixTest(mx);
+    res = connected_components(M, 0);
 
+    std::cout << "Splitted graph:" << std::endl;
 
-	std::cout << std::endl << std::endl;
+    for (auto mx : res)
+        MatrixTest(mx);
 
-	// Disconnect (3, 7)
+    std::cout << std::endl << std::endl;
 
-	M[3][7] = M[7][3] = false;
+    // Disconnect (3, 7)
 
-	std::cout << "3 component graph:" << std::endl;
-	res = connected_components(M, 0);
+    M[3][7] = M[7][3] = false;
 
-	for (auto mx : res)
-		MatrixTest(mx);
+    std::cout << "3 component graph:" << std::endl;
+    res = connected_components(M, 0);
 
+    for (auto mx : res)
+        MatrixTest(mx);
 
-	std::cout << std::endl << std::endl;
+    std::cout << std::endl << std::endl;
 
-	//// Disconnect (7, 8)
-	std::cout << "4 component graph (with 2 null graphs (with 1 vertix)):" << std::endl;
-	M[7][8] = M[8][7] = false;
-	res = connected_components(M, 0);
+    //// Disconnect (7, 8)
+    std::cout << "4 component graph (with 2 null graphs (with 1 vertix)):" << std::endl;
+    M[7][8] = M[8][7] = false;
+    res = connected_components(M, 0);
 
-	for (auto mx : res)
-		MatrixTest(mx);
+    for (auto mx : res)
+        MatrixTest(mx);
 
-
-	std::cout << std::endl << std::endl;
+    std::cout << std::endl << std::endl;
 }
-
