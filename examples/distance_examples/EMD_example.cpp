31ffce124c300068c756364499d21d25acec80d8
stipan <micromillioner@yahoo.com>
2019-08-14 18:46:23 +0200

    Updated EMD example to cinstruct distance matrix between images

--- examples/distance_examples/EMD_example.cpp
+++ examples/distance_examples/EMD_example.cpp
@@ -13,7 +13,42 @@ Copyright (c) 2019 Panda Team
 #include "../../modules/distance.hpp"
 #include <boost/gil/gil_all.hpp> 
 #include <boost/gil/extension/io/jpeg_io.hpp>
+//#include <boost/gil.hpp> 
+//#include <boost/gil/extension/io/jpeg.hpp>
 
+template <typename T>
+void saveToCsv(std::string filename, const std::vector<std::vector<T>> &mat, const std::vector<std::string> &features)
+{
+	std::ofstream outputFile;
+
+	// create and open the .csv file
+	outputFile.open(filename);
+
+	// write the file headers
+	outputFile << ";";
+	for (auto i = 0; i < features.size(); ++i)
+	{
+		outputFile << features[i];
+		outputFile << ";";
+	}
+	//outputFile << "date";
+	outputFile << std::endl;
+
+	// last item in the mat is date
+	for (auto i = 0; i < mat.size(); ++i)
+	{
+		outputFile << features[i];
+		//outputFile << dates[i] << ";";
+		for (auto j = 0; j < mat[i].size(); j++)
+		{
+			outputFile << mat[i][j] << ";";
+		}
+		outputFile << std::endl;
+	}
+
+	// close the output file
+	outputFile.close();
+}
 
 int main()
 {
@@ -24,43 +59,50 @@ int main()
 	std::cout << "we have started" << std::endl; 
 	std::cout << "" << std::endl;
 
-	boost::gil::rgb8_image_t img1;
-	boost::gil::rgb8_image_t img2;
-	boost::gil::jpeg_read_image("assets/sample0.jpg_resized.jpg", img1);
-	boost::gil::jpeg_read_image("assets/sample1.jpg_resized.jpg", img2);
-	std::cout << "Read complete, got an image " << img1.width()
-		<< " by " << img1.height() << " pixels\n";
-	std::cout << "Read complete, got an image " << img2.width()
-		<< " by " << img2.height() << " pixels\n";
+	int num_images = 2;
+	typedef int edm_Type;
 
-	boost::gil::rgb8_pixel_t px = *const_view(img1).at(5, 10);
+	std::vector<std::string> names(num_images);
+	std::vector < boost::gil::rgb8_image_t > jpegs(num_images);
+	std::vector < std::vector < edm_Type > > grayJpegs(num_images);
+	for (size_t i = 0; i < num_images; ++i)
+	{
+		names[i] = "sample_" + std::to_string(i);
+		//boost::gil::read_image("assets/sample" + std::to_string(i) + ".jpg_resized.jpg", jpegs[i], boost::gil::jpeg_tag());
+		boost::gil::jpeg_read_image("assets/sample" + std::to_string(i) + ".jpg_resized.jpg", jpegs[i]);
+		auto gray = boost::gil::color_converted_view<boost::gil::gray8_pixel_t>(const_view(jpegs[i]));
+		for (size_t j = 0; j < gray.size(); ++j)
+		{
+			grayJpegs[i].push_back(gray[j]);
+		}
+	}
+	std::cout << "Read complete, got an image " << jpegs[0].width()
+		<< " by " << jpegs[0].height() << " pixels\n";
+
+	boost::gil::rgb8_pixel_t px = *const_view(jpegs[0]).at(5, 10);
 	std::cout << "The pixel at 5,10 is "
 		<< (int)px[0] << ','
 		<< (int)px[1] << ','
 		<< (int)px[2] << '\n';
-
-	auto gray_img1 = boost::gil::color_converted_view<boost::gil::gray8_pixel_t>(const_view(img1));
-	auto gray_img2 = boost::gil::color_converted_view<boost::gil::gray8_pixel_t>(const_view(img2));
-
-	typedef int edm_Type;
+	
 
 	//size_t im1_R = img1.size() / 6;
 	//size_t im1_C = img1[0].size() / 6;
-	size_t im1_R = img1.width();
-	size_t im1_C = img1.height();
+	size_t im1_R = jpegs[0].width();
+	size_t im1_C = jpegs[0].height();
 
 	// serialize_mat2vec
-	std::vector<edm_Type> i1;
-	std::vector<edm_Type> i2;
+	//std::vector<edm_Type> i1;
+	//std::vector<edm_Type> i2;
 
 	std::cout << "iterate through grayscale" << std::endl;
 	for (size_t i = 0; i < im1_R * im1_C; ++i)
 	{
-		i1.push_back(gray_img1[i]);
-		i2.push_back(gray_img2[i]);
+		//i1.push_back(gray_img1[i]);
+		//i2.push_back(gray_img2[i]);
 	}
 
-	typedef int edm_Type;
+	//typedef int edm_Type;
 
 	//size_t im1_R = img1.size() / 6;
 	//size_t im1_C = img1[0].size() / 6;
@@ -80,7 +122,11 @@ int main()
 	//	}
 	//}
 
+	std::vector < std::vector < edm_Type > > distance_matrix(num_images, std::vector < edm_Type >(num_images));
+
+	std::cout << "create distance matrix" << std::endl;
 	auto cost_mat = metric::EMD_details::ground_distance_matrix_of_2dgrid<edm_Type>(im1_C, im1_R);
+	std::cout << "create max distance matrix" << std::endl;
 	auto maxCost = metric::EMD_details::max_in_distance_matrix(cost_mat);
 
 	std::cout << "we have started" << std::endl;
@@ -88,20 +134,31 @@ int main()
 
 	metric::EMD<edm_Type> distance(cost_mat, maxCost);
 
-	// assumes that i1 and i2 are serialized vectors of the image matrices, and cost_mat contains a distance matrix that takes into account the original pixel locations.
-	auto t1 = std::chrono::steady_clock::now();
-	auto result1 = distance(i1, i2);
-	auto t2 = std::chrono::steady_clock::now();
-	std::cout << "result1: " << result1 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000 << " s)" << std::endl;
-	std::cout << "" << std::endl;
+	for (size_t i = 0; i < num_images; ++i)
+	{
+		for (size_t j = i + 1; j < num_images; ++j)
+		{
+			// assumes that i1 and i2 are serialized vectors of the image matrices, and cost_mat contains a distance matrix that takes into account the original pixel locations.
+			auto t1 = std::chrono::steady_clock::now();
+			auto result1 = distance(grayJpegs[i], grayJpegs[j]);
+			auto t2 = std::chrono::steady_clock::now();
+			std::cout << "result for " << i << " <-> " << j << " is:" << result1 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000 << " s)" << std::endl;
+			std::cout << "" << std::endl;
 
-	std::cout << "swap records and calculate again" << std::endl;
+			distance_matrix[i][j] = result1;
+			distance_matrix[j][i] = result1;
+		}
+	}
+
+	saveToCsv("distances.csv", distance_matrix, names);
+
+	/*std::cout << "swap records and calculate again" << std::endl;
 	std::cout << "" << std::endl;
 
 	auto result2 = distance(i2, i1);
 	auto t3 = std::chrono::steady_clock::now();
 	std::cout << "result2: " << result2 << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count()) / 1000000 << " s)" << std::endl;
-	std::cout << "" << std::endl;
+	std::cout << "" << std::endl;*/
 	
 	return 0;
 }
