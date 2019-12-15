08f5114bcbf0081f63a47359f7232c9b06f97b19
Kataev Victor <kataev.victor.1234@gmail.com>
2019-12-15 15:05:16 +0300

    change to row major in Identity.h

--- modules/utils/dnn/Activation/Identity.h
+++ modules/utils/dnn/Activation/Identity.h
@@ -22,13 +22,13 @@ template<typename Scalar>
 class Identity
 {
 	private:
-		using Matrix = blaze::DynamicMatrix<Scalar, blaze::columnMajor>;
-    public:
+		using Matrix = blaze::DynamicMatrix<Scalar>;
+		using ColumnMatrix = blaze::DynamicMatrix<Scalar, blaze::columnMajor>;
+public:
         // a = activation(z) = z
         // Z = [z1, ..., zn], A = [a1, ..., an], n observations
         static inline void activate(const Matrix& Z, Matrix& A)
         {
-        	/*noalias*/
             A = Z;
         }
 
@@ -38,9 +38,8 @@ class Identity
         // Z = [z1, ..., zn], G = [g1, ..., gn], F = [f1, ..., fn]
         // Note: When entering this function, Z and G may point to the same matrix
         static inline void apply_jacobian(const Matrix& Z, const Matrix& A,
-                                          const Matrix& F, Matrix& G)
+                                          const Matrix& F, ColumnMatrix& G)
         {
-	        /*noalias*/
             G = F;
         }
 };
