3a85d554bb4944be8ae5fa167112d6bbc8249604
Ilya Gavrilov <gilyav@gmail.com>
2020-01-14 09:24:12 +0300

    python: numpy examples; change MGC constructor

--- python/src/metric_py.cpp
+++ python/src/metric_py.cpp
@@ -8,34 +8,10 @@
 
 #include "modules/correlation.hpp"
 #include <boost/python.hpp>
+#include <boost/variant.hpp>
 #include <iostream>
 
 namespace bp = boost::python;
-
-struct simple_user_euclidian {
-
-    double operator()(const std::vector<double>& a, const std::vector<double>& b) const
-    {
-        double sum = 0;
-        for (size_t i = 0; i < a.size(); ++i) {
-            sum += (a[i] - b[i]) * (a[i] - b[i]);
-        }
-        return std::sqrt(sum);
-    }
-    template <class Container>
-    double operator()(const Container& aa, const Container& bb) const
-    {
-        const bp::list& a = bp::extract<bp::list>(aa);
-        const bp::list& b = bp::extract<bp::list>(bb);
-        double sum = 0;
-        for (size_t i = 0; i < bp::len(a); ++i) {
-            double a_i = bp::extract<double>(a[i]);
-            double b_i = bp::extract<double>(b[i]);
-            sum += (a_i - b_i) * (a_i - b_i);
-        }
-        return std::sqrt(sum);
-    }
-};
 
 using base_python_object = bp::api::object;
 class python_wrap_stl: public base_python_object {
@@ -64,27 +40,47 @@ class python_wrap_stl: public base_python_object {
 
 class NotUsed {};
 
-template <class Metric1, class Metric2>
-void bind_MGC(const char* name) {
-    using MGC = metric::MGC<NotUsed, Metric1, NotUsed, Metric2>;
-    bp::class_<MGC>(name)
-        .def("estimate", +[](MGC& self, bp::object& A, bp::object& B, const size_t BsampleSize = 250, const double threshold = 0.05, size_t maxIterations = 1000) {
-            return self.estimate(python_wrap_stl(A), python_wrap_stl(B), BsampleSize, threshold, maxIterations);
-        }, "return estimate of the correlation betweeen A and B")
-        .def("calculate", +[](MGC& self, bp::object& A, bp::object& B) {
-            return self.operator()(python_wrap_stl(A), python_wrap_stl(B));
-        }, "return correlation betweeen A and B");
-}
+class metrics {
+    using MGC_Euclidian_Euclidian = metric::MGC<NotUsed,metric::Euclidian<double>,NotUsed,metric::Euclidian<double>>;
+    using MGC_Euclidian_Manhatten = metric::MGC<NotUsed,metric::Euclidian<double>,NotUsed,metric::Manhatten<double>>;
+    using MGC_Manhatten_Manhatten = metric::MGC<NotUsed,metric::Manhatten<double>,NotUsed,metric::Manhatten<double>>;
+    using MGC_Manhatten_Euclidian = metric::MGC<NotUsed,metric::Manhatten<double>,NotUsed,metric::Euclidian<double>>;
+
+    using mgc_variant = boost::variant<
+            MGC_Euclidian_Euclidian,
+            MGC_Euclidian_Manhatten,
+            MGC_Manhatten_Manhatten,
+            MGC_Manhatten_Euclidian
+        >;
+    static const char constexpr* euclidian = "euclidian";
+    static const char constexpr* manhatten = "manhatten";
+public:
+    std::shared_ptr<mgc_variant> mgc;
+    metrics(const std::string& Metric1, const std::string& Metric2)
+    {
+        if (Metric1 == euclidian && Metric2 == euclidian) mgc = std::make_shared<mgc_variant>(MGC_Euclidian_Euclidian());
+        else if (Metric1 == euclidian && Metric2 == manhatten) mgc = std::make_shared<mgc_variant>(MGC_Euclidian_Manhatten());
+
+        else if (Metric1 == manhatten && Metric2 == manhatten) mgc = std::make_shared<mgc_variant>(MGC_Manhatten_Manhatten());
+        else if (Metric1 == manhatten && Metric2 == euclidian) mgc = std::make_shared<mgc_variant>(MGC_Manhatten_Euclidian());
+
+        else throw std::runtime_error("No such metrics");
+    }
+};
 
 BOOST_PYTHON_MODULE(metric)
 {
+    bp::class_<metrics>("MGC", bp::init<std::string,std::string>())
+        .def("estimate", +[](metrics& self, bp::object& A, bp::object& B, const size_t BsampleSize = 250, const double threshold = 0.05, size_t maxIterations = 1000) {
+            double ret = std::numeric_limits<double>::infinity();
+            boost::apply_visitor([&](auto& obj) { ret = obj.estimate(python_wrap_stl(A), python_wrap_stl(B), BsampleSize, threshold, maxIterations); }, *self.mgc);
+            return ret;
+        }, "return estimate of the correlation betweeen A and B")
+        .def("calculate", +[](metrics& self, bp::object& A, bp::object& B) {
+            double ret = std::numeric_limits<double>::infinity();
+            boost::apply_visitor([&](auto& obj) { ret = obj.operator()(python_wrap_stl(A), python_wrap_stl(B)); }, *self.mgc);
+            return ret;
+        }, "return correlation betweeen A and B");
 
-    bind_MGC<simple_user_euclidian,simple_user_euclidian>("MGC_SimpleEuclidian_SimpleEuclidian");
-
-    bind_MGC<metric::Euclidian<double>,metric::Euclidian<double>>("MGC_Euclidian_Euclidian");
-    bind_MGC<metric::Euclidian<double>,metric::Manhatten<double>>("MGC_Euclidian_Manhatten");
-
-    bind_MGC<metric::Manhatten<double>,metric::Manhatten<double>>("MGC_Manhatten_Manhatten");
-    bind_MGC<metric::Manhatten<double>,metric::Euclidian<double>>("MGC_Manhatten_Euclidian");
 }
 
