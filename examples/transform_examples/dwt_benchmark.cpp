94c77294e67bb619cb45dbd307e1362069d7e20a
maxfilippov <maxfilippov@aol.com>
2020-01-16 09:21:51 +0300

    updated benchmark

--- examples/transform_examples/dwt_benchmark.cpp
+++ examples/transform_examples/dwt_benchmark.cpp
@@ -40,9 +40,40 @@ extract_energies_static(std::vector<T> x)
 
 
 
+template <typename T>
+std::vector<T>
+extract_energies_static_3(std::vector<T> x)
+{
+    auto [x111, x112] = wavelet::dwt(x, 5);
+    auto [x1111, x1112] = wavelet::dwt(x111, 5);
+    auto [x1121, x1122] = wavelet::dwt(x112, 5);
+    auto [x11111, x11112] = wavelet::dwt(x1111, 5);
+    auto [x11121, x11122] = wavelet::dwt(x1112, 5);
+    auto [x11211, x11212] = wavelet::dwt(x1121, 5);
+    auto [x11221, x11222] = wavelet::dwt(x1122, 5);
+    std::vector<std::vector<T>> subbands = {x11111, x11112, x11121, x11122, x11211, x11212, x11221, x11222};
+
+    std::vector<T> energies(subbands.size());
+    for (int i = 0; i < subbands.size(); ++i)
+    {
+        T sum = 0;
+        for (int j = 0; j < subbands[i].size(); ++j)
+        {
+            sum += subbands[i][j] * subbands[i][j];
+        }
+        energies[i] = sum / T(subbands[0].size());
+    }
+    //std::cout << subbands[0].size() << std::endl;
+    return energies;
+}
+
+
+
+
+
 int main() {
 
-    size_t runs = 100; // set number of runs
+    size_t runs = 1000; // set number of runs
     size_t len  = 48000; // set length of test random vector
 
     std::random_device dev;
@@ -89,7 +120,7 @@ int main() {
     sum = 0;
     for (size_t i = 0; i<runs; ++i) {
         t1 = std::chrono::steady_clock::now();
-        result2 = extract_energies_static(x);
+        result2 = extract_energies_static_3(x);
         t2 = std::chrono::steady_clock::now();
         sum += double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000;
     }
