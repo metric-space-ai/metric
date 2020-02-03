c9bd8a8ee36e38d6c99d57282a6f90447e430dd3
stipan <micromillioner@yahoo.com>
2020-02-03 17:48:52 +0200

    [Kolmogorov-Smirnov] added implementation for Kolmogorov-Smirnov distance

---
+++ examples/distance_examples/kolmogorov_smirnov_ditance_example.cpp
@@ -0,0 +1,39 @@
+﻿/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2019 Panda Team
+*/
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include "../../modules/distance.hpp"
+#include "assets/test_data.cpp"
+
+
+int main()
+{
+	    /******************** examples for Kolmogorov-Smirnov Distance **************************/
+    // example for picture
+    std::cout << "Kolmogorov-Smirnov distance example have started" << std::endl;
+    std::cout << "" << std::endl;	
+
+	/*** here are some data records ***/
+	std::vector<double> distribution_1 = { 0, 1, 2, 3, 4, 3, 2, 1 };
+	std::vector<double> distribution_2 = { 0, 1, 2, 3, 3, 2, 1, 0 };
+	   
+    metric::KolmogorovSmirnov<std::vector<double>, double> distance;
+
+    // assumes that i1 and i2 are serialized vectors of the image matrices, and cost_mat contains a distance matrix that takes into account the original pixel locations.
+    auto t1 = std::chrono::steady_clock::now();
+    auto result1 = distance(distribution_1, distribution_2);
+    auto t2 = std::chrono::steady_clock::now();
+    std::cout << "result1: " << result1
+              << " (Time = " << double(std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()) / 1000000
+              << " s)" << std::endl;
+    std::cout << "" << std::endl;
+
+    return 0;
+}
---
+++ modules/distance/k-random/KolmogorovSmirnov.cpp
@@ -0,0 +1,96 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2020 Panda Team
+*/
+#ifndef _METRIC_DISTANCE_K_RANDOM_KOLMOGOROV_SMIRNOV_CPP
+#define _METRIC_DISTANCE_K_RANDOM_KOLMOGOROV_SMIRNOV_CPP
+
+#include "KolmogorovSmirnov.hpp"
+
+namespace metric {
+	
+
+template <typename Sample, typename D>
+auto KolmogorovSmirnov<typename Sample, typename D>::operator()(const Sample& sample_1, const Sample& sample_2) -> distance_return_type
+{
+	Sample normalized_distribution_1(sample_1.size());
+	Sample normalized_distribution_2(sample_2.size());
+	Sample cumulitive_distribution_1;
+	Sample cumulitive_distribution_2;
+
+	// make cumulative distribution functions for the first sample
+
+	Sample::value_type min_value = INFINITY;
+	Sample::value_type max_value = -INFINITY; 
+	Sample::value_type cumulitive_sum = 0;
+	for (int i = 0; i < sample_1.size(); i++)
+	{
+		if (sample_1[i] > max_value)
+		{
+			max_value = sample_1[i];
+		}
+		if (sample_1[i] < min_value)
+		{
+			min_value = sample_1[i];
+		}
+	}
+
+	for (int i = 0; i < sample_1.size(); i++)
+	{
+		normalized_distribution_1[i] = (sample_1[i] - min_value) / (max_value - min_value);
+	}
+	for (int i = 0; i < normalized_distribution_1.size(); i++)
+	{
+		cumulitive_sum += normalized_distribution_1[i];
+		cumulitive_distribution_1.push_back(cumulitive_sum);
+	}
+	
+	// make cumulative distribution functions for the second sample
+	
+	min_value = INFINITY;
+	max_value = -INFINITY;
+	cumulitive_sum = 0;
+	for (int i = 0; i < sample_2.size(); i++)
+	{
+		if (sample_2[i] > max_value)
+		{
+			max_value = sample_2[i];
+		}
+		if (sample_2[i] < min_value)
+		{
+			min_value = sample_2[i];
+		}
+	}
+
+	for (int i = 0; i < sample_2.size(); i++)
+	{
+		normalized_distribution_2[i] = (sample_2[i] - min_value) / (max_value - min_value);
+	}
+	for (int i = 0; i < normalized_distribution_2.size(); i++)
+	{
+		cumulitive_sum += normalized_distribution_2[i];
+		cumulitive_distribution_2.push_back(cumulitive_sum);
+	}
+	
+	// find the most difference between distributions
+	
+	D max_difference = 0;
+	for (int i = 0; i < cumulitive_distribution_1.size(); i++)
+	{
+		std::cout << cumulitive_distribution_1[i] << " " << cumulitive_distribution_2[i] << " " << (cumulitive_distribution_1[i] - cumulitive_distribution_2[i]) << std::endl;
+		if (abs(cumulitive_distribution_1[i] - cumulitive_distribution_2[i]) > max_difference)
+		{
+			max_difference = abs(cumulitive_distribution_1[i] - cumulitive_distribution_2[i]);
+		}
+	}
+
+
+    return max_difference;
+}
+
+
+}  // namespace metric
+#endif
