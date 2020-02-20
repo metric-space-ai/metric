39fcf98bfe08e4ace7e495dc4a6535f3e6418972
jura-gresko <juragresko@gmail.com>
2020-02-20 04:45:07 -0800

    add KOC factory and example to test

---
+++ python/src/metric_KOC.cpp
@@ -0,0 +1,86 @@
+#include "modules/mapping.hpp"
+
+#include <boost/python.hpp>
+
+namespace bp = boost::python;
+
+template< typename T >
+inline
+std::vector< T > to_std_vector( const bp::list& iterable )
+{
+    return std::vector< T >( bp::stl_input_iterator< T >( iterable ),
+                             bp::stl_input_iterator< T >( ) );
+}
+
+// TODO: template with record type
+void export_metric_KOC() {
+    using Record = std::vector<double>;
+    using Factory = metric::KOC_factory<Record>;
+
+    auto factory = bp::class_<Factory>("KOC_factory");
+
+    factory.def(
+        bp::init<size_t, double, double, size_t, Factory::T, Factory::T>(
+            (
+                bp::arg("nodesNumber"),
+                bp::arg("start_learn_rate")= 0.8,
+                bp::arg("finish_learn_rate") = 0.0,
+                bp::arg("iterations") = 20,
+                bp::arg("distribution_min") = -1,
+                bp::arg("distribution_max") = 1
+            )
+        )
+    );
+    factory.def(
+        bp::init<size_t, size_t, double, double, size_t, Factory::T, Factory::T>(
+            (
+                bp::arg("nodesWidth") = 5,
+                bp::arg("nodesHeight") = 4,
+                bp::arg("start_learn_rate") = 0.8,
+                bp::arg("finish_learn_rate") = 0.0,
+                bp::arg("iterations") = 20,
+                bp::arg("distribution_min") = -1,
+                bp::arg("distribution_max") = 1
+            )
+        )
+    );
+    factory.def(
+        bp::init<size_t, double, double, size_t, Factory::T, Factory::T, double, double, long long>(
+            (
+                bp::arg("nodesNumber"),
+                bp::arg("start_learn_rate"),
+                bp::arg("finish_learn_rate"),
+                bp::arg("iterations"),
+                bp::arg("distribution_min"),
+                bp::arg("distribution_max"),
+                bp::arg("neighborhood_start_size"),
+                bp::arg("neigbour_range_decay"),
+                bp::arg("random_seed")
+            )
+        )
+    );
+    factory.def(
+        bp::init<size_t, size_t, double, double, size_t, Factory::T, Factory::T, double, double, long long>(
+            (
+                bp::arg("nodesWidth"),
+                bp::arg("nodesHeight"),
+                bp::arg("start_learn_rate"),
+                bp::arg("finish_learn_rate"),
+                bp::arg("iterations"),
+                bp::arg("distribution_min"),
+                bp::arg("distribution_max"),
+                bp::arg("neighborhood_start_size"),
+                bp::arg("neigbour_range_decay"),
+                bp::arg("random_seed")
+            )
+        )
+    );
+
+    factory.def("__call__", &Factory::operator(), "construct KOC");
+
+    // KOC
+    auto koc = bp::class_<Factory::KOC>("KOC", bp::no_init);
+
+}
+
+// make build && pip uninstall metric && pip install dist/metric-0.0.1-cp36-cp36m-linux_x86_64.whl
