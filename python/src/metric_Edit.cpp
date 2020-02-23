1ae7b17680c17ff2855301f73f49d3b192abb1ad
jura-gresko <juragresko@gmail.com>
2020-02-23 13:16:10 -0800

    add Edit distance

--- python/src/metric_Edit.cpp
+++ python/src/metric_Edit.cpp
@@ -1,31 +1,23 @@
-/*
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-  Copyright (c) 2020 Panda Team
-*/
-
 #include "metric_py.hpp"
 #include "modules/distance.hpp"
 
+#include <string>
+#include <vector>
+
 namespace bp = boost::python;
 
-class NotUsed {};
-class metricsEdit {
-    metric::Edit<NotUsed> metricEdit;
-
-public:
-    int call(const WrapStlMatrix<double>& A, const WrapStlMatrix<double>& B) {
-        return metricEdit(A,B);
-    }
-};
+template<typename Value>
+void register_wrapper_edit() {
+    using Metric = metric::Edit<Value>;
+    auto p1 = &Metric::template operator()<std::basic_string_view<Value>>;
+    auto p2 = &Metric::template operator()<std::vector<double>>;
+    int (Metric::*p3)(const Value*, const Value*) const = &Metric::operator();
+    bp::class_<Metric>("Edit")
+        .def("__call__", p1)
+        .def("__call__", p2)
+        .def("__call__", p3);
+}
 
 void export_metric_Edit() {
-
-    bp::class_<metricsEdit>("Edit")
-        .def("__call__", +[](metricsEdit& self, bp::object& A, bp::object& B) {
-            return self.call(A,B);
-        },
-    "return Edit distance between A and B");
+    register_wrapper_edit<char>();
 }
