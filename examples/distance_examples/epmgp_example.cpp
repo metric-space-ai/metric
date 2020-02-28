b00589ee5700fde003ecd0cd0c966ea150bac294
maxfilippov <maxfilippov@aol.com>
2020-02-28 12:13:39 +0300

    added erfcx approximation

--- examples/distance_examples/epmgp_example.cpp
+++ examples/distance_examples/epmgp_example.cpp
@@ -4,15 +4,100 @@
 #include <tuple>
 #include "../../modules/distance/k-random/epmgp.cpp"
 
+#include <limits>
+
 
 
 int main() {
 
     /*
-    std::vector<double> lower = {-0.5, -0.5};
-    std::vector<double> upper = {0.5, 0.5};
-    std::vector<double> mu = {0, 0};
-    std::vector<double> sigma = {1, 1};
+
+    for (double a = -30; a < 530; a += 1) {
+        //std::cout << a << " " << std::log(0.5) + std::log(epmgp::erfcx(a)) - a*a << "\n";
+        std::cout << a << " " << epmgp::erfcx(a) << " " << epmgp::erfcx_simple(a) << "\n";
+    }
+    // */
+
+    /*
+
+    //for (int x = 5; x>-30; x -= 1) {
+    for (double a = -50; a<50; a += 1) {
+//        auto logZhat1 = std::log(1 - std::exp( std::log(0.5) + std::log(epmgp::erfcx_simple(-a)) - a*a ));
+//        auto logZhat2 = std::log(0.5) + std::log(epmgp::erfcx_simple(a)) - a*a;
+//        auto logZhat1d = std::log(1 - std::exp( std::log(0.5) + std::log(epmgp::erfcx_double(-a)) - a*a ));
+//        auto logZhat2d = std::log(0.5) + std::log(epmgp::erfcx_double(a)) - a*a;
+//        auto errMinus = epmgp::erfcx(-a);
+//        auto errPlus = epmgp::erfcx(a);
+//        auto logErrPlus = std::log(errPlus);
+//        auto logErrMinus = std::log(errMinus);
+//        auto expArg = std::log(0.5) + std::log(epmgp::erfcx(-a)) - a*a;
+//        auto expResult = std::exp( std::log(0.5) + std::log(epmgp::erfcx(-a)) - a*a );
+        //std::cout << "x: " << x << ", erfcx(x): " << epmgp::erfcx(x) << "\n";
+
+        double logZhat;
+        if (a < -26) {
+            auto logZhatOtherTail = std::log(0.5) + std::log(epmgp::erfcx(-a)) - a*a;
+            logZhat = std::log(1 - std::exp(logZhatOtherTail));
+            ////std::cout << "log(erfcx(-a)) " << a << " " << std::log(erfcx(-a)) << "\n"; // TODO remove
+            //logZhat = 0; // lim[-Inf](logZhat) = 0
+        } else {
+            //double long_a = a;
+            //logZhat = std::log(0.5) + std::log(epmgp::erfcx_double(long_a)) - long_a*long_a;
+            logZhat = std::log(0.5) + std::log(epmgp::erfcx(a)) - a*a;
+        }
+
+//        double b = a;
+//        if (b > 26) {
+//            auto logZhatOtherTail = std::log(0.5) + std::log(epmgp::erfcx(b)) - b*b;
+//            logZhat = std::log(1 - std::exp(logZhatOtherTail));
+//        }
+//        else
+//            logZhat = std::log(0.5) + std::log(epmgp::erfcx(-b)) - b*b;
+        std::cout <<
+                     "x: " << a <<
+                     ", logZhat: " << logZhat <<
+//                     ", logZhat1: " << logZhat1 <<
+//                     ", logZhat2: " << logZhat2 <<
+//                     ", logZhat1d: " << logZhat1d <<
+//                     ", logZhat2d: " << logZhat2d <<
+//                     ", errMinus: " << errMinus <<
+//                     ", errPlus: " << errPlus <<
+//                     ", logErrMinus: " << logErrMinus <<
+//                     ", logErrPlus: " << logErrPlus <<
+//                     ", expArg: " << expArg <<
+//                     ", expResult: " << expResult <<
+//                     ", logArg: " << 1 - std::exp( std::log(0.5) + std::log(epmgp::erfcx(-a)) - a*a ) <<
+//                     ", std::erfc(a): " << std::erfc(a) <<
+//                     ", std::exp(a*a): " << std::exp(a*a) <<
+                     "\n";
+    }
+    // */
+
+    /*
+
+    //std::vector<double> lower = {-0.5, -0.5};
+    //std::vector<double> upper = {0.5, 0.5};
+    //std::vector<double> mu = {0, 0};
+    //std::vector<double> sigma = {1, 1};
+
+    double inf = std::numeric_limits<double>::infinity();
+
+//    std::vector<double> lower = {-0.5, -0.5, 0.5};
+//    std::vector<double> upper = {0.5, 0.5, inf};
+//    std::vector<double> mu = {0, 0, 0};
+//    std::vector<double> sigma = {1, 0, 1};
+
+//    std::vector<double> lower = {-inf};
+//    std::vector<double> upper = {0.5};
+//    std::vector<double> mu = {-100};
+//    std::vector<double> sigma = {1};
+
+    std::vector<double> lower = {-50};
+    std::vector<double> upper = {60};
+    std::vector<double> mu = {0};
+    std::vector<double> sigma = {1};
+
+
 
     auto result = epmgp::truncNormMoments(lower, upper, mu, sigma);
 
@@ -26,15 +111,28 @@ int main() {
     }
     // */
 
+
+    //*
+
 //    blaze::DynamicVector<double> m = {0, 0};
 //    blaze::DynamicMatrix<double> K = {{1, 0}, {0, 1}};
 //    blaze::DynamicVector<double> lowerB = {-0.5, -0.5};
 //    blaze::DynamicVector<double> upperB = {0.5, 0.5};
 
-    blaze::DynamicVector<double> m = {5, -5};
-    blaze::DynamicMatrix<double> K = {{1, 0.3}, {0.3, 1}};
-    blaze::DynamicVector<double> lowerB = {0.5, 1.5};
-    blaze::DynamicVector<double> upperB = {1.5, 2.5};
+//    blaze::DynamicVector<double> m = {5, -5};
+//    blaze::DynamicMatrix<double> K = {{1, 0.3}, {0.3, 1}};
+//    blaze::DynamicVector<double> lowerB = {0.5, 1.5};
+//    blaze::DynamicVector<double> upperB = {1.5, 2.5};
+
+    blaze::DynamicVector<double> m = {-250, -250};
+    blaze::DynamicMatrix<double> K = {{1, 0}, {0, 1}};
+    blaze::DynamicVector<double> lowerB = {200, 200};
+    blaze::DynamicVector<double> upperB = {30000, 30000};
+
+//    blaze::DynamicVector<double> m = {5, -5, -3};
+//    blaze::DynamicMatrix<double> K = {{1, 0.3, 0.7}, {0.3, 1, 0.5}, {0.7, 0.5, 1}};
+//    blaze::DynamicVector<double> lowerB = {0.5, 1.5, 0};
+//    blaze::DynamicVector<double> upperB = {1.5, 2.5, 3};
 
     auto result2 = epmgp::local_gaussian_axis_aligned_hyperrectangles(m, K, lowerB, upperB);
 
@@ -45,6 +143,8 @@ int main() {
     std::cout << "\n\logZ2 = " << logZ2 << "\n";
     std::cout << "muOUT2 = \n" << muOUT2 << "\n";
     std::cout << "sigmaOUT2 = \n" << sigmaOUT2 << "\n";
+
+    // */
 
     return 0;
 }
--- modules/distance/k-random/epmgp.cpp
+++ modules/distance/k-random/epmgp.cpp
@@ -10,8 +10,8 @@ Copyright (c) 2019 Panda Team
 // https://arxiv.org/pdf/1111.6832.pdf
 // based on local_gaussian.m Matlab code
 
-#ifndef _METRIC_DISTANCE_K_RANDOM_VOI_CPP
-#define _METRIC_DISTANCE_K_RANDOM_VOI_CPP
+#ifndef _EPMGP_CPP
+#define _EPMGP_CPP
 
 #include <vector>
 #include <tuple>
@@ -38,9 +38,86 @@ int sgn(T val) { // sign for arbitrary type
 
 
 template <typename T>
-T erfcx(T x) {
+T erfcx_simple(T x) { // for double on x86_64, inf starts at -26
     return std::exp(x*x)*std::erfc(x);
 }
+
+
+// https://stackoverflow.com/questions/39777360/accurate-computation-of-scaled-complementary-error-function-erfcx
+double erfcx_double (double x)
+{
+    double a, d, e, m, p, q, r, s, t;
+
+    a = fmax (x, 0.0 - x); // NaN preserving absolute value computation
+
+    /* Compute q = (a-4)/(a+4) accurately. [0,INF) -> [-1,1] */
+    m = a - 4.0;
+    p = a + 4.0;
+    r = 1.0 / p;
+    q = m * r;
+    t = fma (q + 1.0, -4.0, a);
+    e = fma (q, -a, t);
+    q = fma (r, e, q);
+
+    /* Approximate (1+2*a)*exp(a*a)*erfc(a) as p(q)+1 for q in [-1,1] */
+    p =             0x1.edcad78fc8044p-31;  //  8.9820305531190140e-10
+    p = fma (p, q,  0x1.b1548f14735d1p-30); //  1.5764464777959401e-09
+    p = fma (p, q, -0x1.a1ad2e6c4a7a8p-27); // -1.2155985739342269e-08
+    p = fma (p, q, -0x1.1985b48f08574p-26); // -1.6386753783877791e-08
+    p = fma (p, q,  0x1.c6a8093ac4f83p-24); //  1.0585794011876720e-07
+    p = fma (p, q,  0x1.31c2b2b44b731p-24); //  7.1190423171700940e-08
+    p = fma (p, q, -0x1.b87373facb29fp-21); // -8.2040389712752056e-07
+    p = fma (p, q,  0x1.3fef1358803b7p-22); //  2.9796165315625938e-07
+    p = fma (p, q,  0x1.7eec072bb0be3p-18); //  5.7059822144459833e-06
+    p = fma (p, q, -0x1.78a680a741c4ap-17); // -1.1225056665965572e-05
+    p = fma (p, q, -0x1.9951f39295cf4p-16); // -2.4397380523258482e-05
+    p = fma (p, q,  0x1.3be1255ce180bp-13); //  1.5062307184282616e-04
+    p = fma (p, q, -0x1.a1df71176b791p-13); // -1.9925728768782324e-04
+    p = fma (p, q, -0x1.8d4aaa0099bc8p-11); // -7.5777369791018515e-04
+    p = fma (p, q,  0x1.49c673066c831p-8);  //  5.0319701025945277e-03
+    p = fma (p, q, -0x1.0962386ea02b7p-6);  // -1.6197733983519948e-02
+    p = fma (p, q,  0x1.3079edf465cc3p-5);  //  3.7167515521269866e-02
+    p = fma (p, q, -0x1.0fb06dfedc4ccp-4);  // -6.6330365820039094e-02
+    p = fma (p, q,  0x1.7fee004e266dfp-4);  //  9.3732834999538536e-02
+    p = fma (p, q, -0x1.9ddb23c3e14d2p-4);  // -1.0103906603588378e-01
+    p = fma (p, q,  0x1.16ecefcfa4865p-4);  //  6.8097054254651804e-02
+    p = fma (p, q,  0x1.f7f5df66fc349p-7);  //  1.5379652102610957e-02
+    p = fma (p, q, -0x1.1df1ad154a27fp-3);  // -1.3962111684056208e-01
+    p = fma (p, q,  0x1.dd2c8b74febf6p-3);  //  2.3299511862555250e-01
+
+    /* Divide (1+p) by (1+2*a) ==> exp(a*a)*erfc(a) */
+    d = a + 0.5;
+    r = 1.0 / d;
+    r = r * 0.5;
+    q = fma (p, r, r); // q = (p+1)/(1+2*a)
+    t = q + q;
+    e = (p - q) + fma (t, -a, 1.0); // residual: (p+1)-q*(1+2*a)
+    r = fma (e, r, q);
+
+    /* Handle argument of infinity */
+    if (a > 0x1.fffffffffffffp1023) r = 0.0;
+
+    /* Handle negative arguments: erfcx(x) = 2*exp(x*x) - erfcx(|x|) */
+    if (x < 0.0) {
+        s = x * x;
+        d = fma (x, x, -s);
+        e = exp (s);
+        r = e - r;
+        r = fma (e, d + d, r);
+        r = r + e;
+        if (e > 0x1.fffffffffffffp1023) r = e; // avoid creating NaN
+    }
+    return r;
+}
+
+
+
+template <typename T>
+T erfcx(T x) { // for double on x86_64, inf starts at -26
+    return (T)erfcx_double((double)x);
+}
+
+
 
 
 // TODO make template
@@ -107,6 +184,7 @@ truncNormMoments(
                         if (a < -26) {
                             auto logZhatOtherTail = std::log(0.5) + std::log(erfcx(-a)) - a*a;
                             logZhat = std::log(1 - std::exp(logZhatOtherTail));
+                            //logZhat = 0; // lim[-Inf]logZhat = 0
                         } else {
                             logZhat = std::log(0.5) + std::log(erfcx(a)) - a*a;
                         }
@@ -286,4 +364,5 @@ local_gaussian_axis_aligned_hyperrectangles(
 
 } // namespace
 
-#endif
+#endif  // _EPMGP_CPP
+
