c5834f8eac4d0c5c06d700312582ecdfafe11d7e
jura-gresko <juragresko@gmail.com>
2020-03-05 13:53:57 -0300

    update Matrix wrapper and fix build

--- python/src/space/matrix.cpp
+++ python/src/space/matrix.cpp
@@ -3,34 +3,38 @@
 
 #include <boost/python.hpp>
 #include <vector>
+#include <pair>
 #include <iostream>
 
 namespace bp = boost::python;
 
 // TODO: add metric
-template<typename recType>
+template<typename recType, typename Metric>
 void register_wrapper_matrix() {
-    using Matrix = metric::Matrix<recType>;
-    // using Container = std::vector<recType>;
-    // bool (Matrix::*append1)(const recType&) = &Matrix::append;
-    // bool (Matrix::*append_if1)(const recType&, float) = &Matrix::append_if;
-    // bool (Matrix::*append2)(const Container&) = &Matrix::append;
-    // bool (Matrix::*append_if2)(const Container&, float) = &Matrix::append_if;
+    using Matrix = metric::Matrix<recType, Metric>;
+    using Container = std::vector<recType>;
+    size_t (Matrix::*insert1)(const recType&) = &Matrix::insert;
+    std::pair<std::size_t, bool> (Matrix::*insert_if1)(const recType&, Matrix::distType) = &Matrix::insert_if;
+    auto insert2 = &Matrix::insert<Container>;
+    auto inset_if2 = &Matrix::insert_if<Container>;
 
-    bp::class_<Matrix>("Matrix", bp::init<const std::vector<recType>&>())
-        // FIXME: missing in CPP
-        //.def(bp::init<const recType&>()
-        //.def("append", append1)
-        //.def("append_if", append_if1)
-        //.def("append", append2)
-        //.def("append_if", append_if2)
-        // .def("erase", &Matrix::erase)
-        //.def("set", &Matrix::set)
-        //.def("__getitem__", &Matrix::operator[])  // FIXME: this is broken in CPP
+    bp::class_<Matrix>("Matrix", bp::init<const Container&>())
+        .def(bp::init<const recType&>()
+        .def("insert", insert1)
+        .def("insert_if", insert_if1)
+        .def("insert", insert2)
+        .def("insert_if", inset_if2)
+        .def("erase", &Matrix::erase)
+        .def("set", &Matrix::set)
+        .def("__getitem__", &Matrix::operator[])
+        .def("__setitem__", &Metric::set)
         .def("__call__", &Matrix::operator())
-        .def("size", &Matrix::size);
+        .def("erase", &Matrix::erase)
+        .def("size", &Matrix::size)
+        .def("nn", &Matrix::nn)
+        .def("knn", &Matrix::knn)
+        .def("rnn", &Matrix::rnn);
 }
-
 
 void export_metric_matrix() {
     register_wrapper_matrix<std::vector<double>>();
