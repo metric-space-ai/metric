21fb37028ed1f22ce96255fa3a369c335a690e6e
jura-gresko <juragresko@gmail.com>
2020-03-05 17:35:32 -0300

    fix Matrix, deactivate Tree wrapper for now

--- python/src/space/matrix.cpp
+++ python/src/space/matrix.cpp
@@ -1,9 +1,10 @@
 #include "modules/space/matrix.hpp"
+#include "modules/distance.hpp"
 #include "metric_py.hpp"
 
 #include <boost/python.hpp>
 #include <vector>
-#include <pair>
+#include <utility>
 #include <iostream>
 
 namespace bp = boost::python;
@@ -14,20 +15,20 @@ void register_wrapper_matrix() {
     using Matrix = metric::Matrix<recType, Metric>;
     using Container = std::vector<recType>;
     size_t (Matrix::*insert1)(const recType&) = &Matrix::insert;
-    std::pair<std::size_t, bool> (Matrix::*insert_if1)(const recType&, Matrix::distType) = &Matrix::insert_if;
-    auto insert2 = &Matrix::insert<Container>;
-    auto inset_if2 = &Matrix::insert_if<Container>;
+    std::vector<size_t> (Matrix::*insert2)(const Container&) = &Matrix::insert;
+    std::pair<std::size_t, bool> (Matrix::*insert_if1)(const recType&, typename Matrix::distType) = &Matrix::insert_if;
+    std::vector<std::pair<std::size_t, bool>> (Matrix::*insert_if2)(const Container&, typename Matrix::distType) = &Matrix::insert_if;
 
     bp::class_<Matrix>("Matrix", bp::init<const Container&>())
-        .def(bp::init<const recType&>()
+        .def(bp::init<const recType&>())
         .def("insert", insert1)
         .def("insert_if", insert_if1)
         .def("insert", insert2)
-        .def("insert_if", inset_if2)
+        .def("insert_if", insert_if2)
         .def("erase", &Matrix::erase)
         .def("set", &Matrix::set)
-        .def("__getitem__", &Matrix::operator[])
-        .def("__setitem__", &Metric::set)
+        //.def("__getitem__", &Matrix::operator[]) // FIXME: broken in CPP
+        .def("__setitem__", &Matrix::set)
         .def("__call__", &Matrix::operator())
         .def("erase", &Matrix::erase)
         .def("size", &Matrix::size)
@@ -37,5 +38,5 @@ void register_wrapper_matrix() {
 }
 
 void export_metric_matrix() {
-    register_wrapper_matrix<std::vector<double>>();
+    register_wrapper_matrix<std::vector<double>, metric::Euclidian<double>>();
 }
--- python/src/todo/tree.cpp
+++ python/src/todo/tree.cpp
@@ -9,82 +9,57 @@ namespace bp = boost::python;
 template <class recType, class Metric>
 class Tree {
 */
-    typedef Node<recType, Metric> NodeType;
-    typedef Node<recType, Metric>* Node_ptr;
-    typedef Tree<recType, Metric> TreeType;
-    using rset_t = std::tuple<Node_ptr, std::vector<Node_ptr>, std::vector<Node_ptr>>;
-    using Distance = typename std::result_of<Metric(recType, recType)>::type;
+    std::vector<std::vector<std::size_t>> clustering(
+        const std::vector<double>& distribution,
+        const std::vector<std::size_t>& indexes, const std::vector<recType>& points
+    );
 
-    std::vector<std::vector<std::size_t>> clustering(const std::vector<double>& distribution, const std::vector<std::size_t>& indexes, const std::vector<recType>& points);
-    std::vector<std::vector<std::size_t>> clustering(const std::vector<double>& distribution, const std::vector<std::size_t>& IDS);
-    std::vector<std::vector<std::size_t>> clustering(const std::vector<double>& distribution, const std::vector<recType>& points);
+    std::vector<std::vector<std::size_t>> clustering(
+        const std::vector<double>& distribution, const std::vector<std::size_t>& IDS
+    );
+
+    std::vector<std::vector<std::size_t>> clustering(
+        const std::vector<double>& distribution, const std::vector<recType>& points
+    );
 
     template <class Archive, class Stream> void deserialize(Archive& input, Stream& stream);
     template <class Archive> void serialize(Archive& archive);
 
-    /*** Access Operations ***/
     std::size_t insert(const recType& p);
-
     bool insert(const std::vector<recType>& p);
-
     std::tuple<std::size_t, bool> insert_if(const recType& p, Distance treshold);
     std::size_t insert_if(const std::vector<recType>& p, Distance treshold);
     bool erase(const recType& p);
     recType operator[](size_t id);
-
     Node_ptr nn(const recType& p) const;
     std::vector<std::pair<Node_ptr, Distance>> knn(const recType& p, unsigned k = 10) const;
     std::vector<std::pair<Node_ptr, Distance>> rnn(const recType& p, Distance distance = 1.0) const;
-
-    /*** utilitys ***/
     size_t size();
     void traverse(const std::function<void(Node_ptr)>& f);
-
     int levelSize();
     void print() const;
+    void print(std::ostream& ostr) const;
     std::map<int, unsigned> print_levels();
     std::vector<recType> toVector();
     std::string to_json(std::function<std::string(const recType&)> printer);
-
     std::string to_json();
     bool check_covering() const;
-
     void traverse_child(const std::function<void(Node_ptr)>& f);
-    Node_ptr get_root() { return root; }
-
+    Node_ptr get_root();
     bool empty() const { return root == nullptr; }
-
     bool same_tree(const Node_ptr lhs, const Node_ptr rhs) const;
-    bool operator==(const Tree& t) const { return same_tree(root, t.root); }
+    bool operator==(const Tree& t);
     friend std::ostream& operator<<(std::ostream& ostr, const Tree& t);
+    Distance distance_by_id(std::size_t id1, std::size_t id2) const;
+    Distance distance(const recType & p1, const recType & p2) const;
+    blaze::SymmetricMatrix<blaze::DynamicMatrix<Distance, blaze::rowMajor>> matrix() const;
 */
 
 template <typename recType, typename Metric>
 void register_wrapper_Tree() {
     using Tree = metric::Tree<recType, Metric>;
+    using Container = std::vector<recType>;
     auto tree = bp::class_<Tree>("Tree", bp::no_init);
-
-    //Tree(int truncate = -1, Metric d = Metric());  // empty tree
-
-    /**
-     * @brief Construct a Tree object with one data record as root
-     *
-     * @param p data record
-     * @param truncate truncate parameter
-     * @param d metric object
-     */
-    //Tree(const recType& p, int truncate = -1, Metric d = Metric());  // cover tree with one data record as root
-
-    /**
-     * @brief Construct a Tree object from Container
-     *
-     * @param p vector of data records to store in tree
-     * @param truncate truncate paramter
-     * @param d metric object
-     */
-    //template <class Container>
-    //Tree(const Container& p, int truncateArg = -1, Metric d = Metric());
-
     tree.def(bp::init<int, Metric>(
         (
             bp::arg("truncate") = -1,
@@ -99,11 +74,15 @@ void register_wrapper_Tree() {
             bp::arg("distance") = Metric(),
         )
     ));
+    tree.def(bp::init<const Container&, int, Metric>(
+        (
+            bp::arg("p"),
+            bp::arg("truncate") = -1,
+            bp::arg("distance") = Metric(),
+        )
+    ));
     tree.def("clustering",)
     // template <class Container> Tree(const Container& p, int truncateArg = -1, Metric d = Metric()); // TODO
-
-    Matrix (Mapping::*encode1) (const std::vector<recType>&) = &Mapping::time_freq_PCFA_encode;
-    Matrix (Mapping::*encode2) (const std::tuple<std::deque<std::vector<recType>>, std::deque<std::vector<recType>>>) = &Mapping::time_freq_PCFA_encode;
     dspcc.def("time_freq_PCFA_encode", encode1);
     dspcc.def("time_freq_PCFA_encode", encode2);
     dspcc.def("time_freq_PCFA_decode", &Mapping::time_freq_PCFA_decode);
