0c0081072d0dd3cc301b7a60a704b1fa67e1a120
jura-gresko <juragresko@gmail.com>
2020-03-09 14:01:11 -0300

    + MGC_direct

--- python/src/correlation/MGC.cpp
+++ python/src/correlation/MGC.cpp
@@ -35,14 +35,19 @@ void wrap_metric_MGC() {
             return self.estimate(a, b, b_sample_size, threshold, max_iterations);
         }
     );
-    std::vector<double> (Metric::*xcorr)(const Container&, const Container&) = &Metric::xcorr;
-    mgc.def("xcorr", xcorr);
-
-    mgc.def("compute_distance_matrix", +[](Metric& self,
-        const WrapStlMatrix<double>& c) {
-            return self.computeDistanceMatrix(c);
+    mgc.def("xcorr", +[](Metric& self,
+        const WrapStlMatrix<double>& a,
+        const WrapStlMatrix<double>& b,
+        int n) {
+            return self.xcorr(a, b, n);
         }
     );
+
+//    mgc.def("compute_distance_matrix", +[](Metric& self,  FIXME: Metric parameter undefined
+//        const WrapStlMatrix<double>& c) {
+//            return self.computeDistanceMatrix(c);
+//        }
+//    );
     mgc.def("mean", &Metric::mean);
     mgc.def("variance", &Metric::variance);
     mgc.def("icdf", &Metric::icdf);
@@ -53,7 +58,30 @@ void wrap_metric_MGC() {
     mgc.def("linspace", &Metric::linspace);
 }
 
+template <class T>
+void wrap_metric_MGC_direct() {
+    using Metric = metric::MGC_direct;
+    auto mgc = bp::class_<Metric>("MGC_direct", bp::no_init);
+    mgc.def("__call__", &Metric::operator()<T>);   // FIXME: unsupported argument types
+    mgc.def("xcorr", &Metric::xcorr<T>);           // FIXME: unsupported argument types
+    mgc.def("center_distance_matrix", &Metric::center_distance_matrix<T>);
+    mgc.def("rank_distance_matrix", &Metric::rank_distance_matrix<T>);
+    mgc.def("rank_distance_matrix", &Metric::rank_distance_matrix<T>);
+    mgc.def("center_ranked_distance_matrix", &Metric::center_ranked_distance_matrix<T>);
+    mgc.def("local_covariance", &Metric::local_covariance<T>);
+    mgc.def("normalize_generalized_correlation", &Metric::normalize_generalized_correlation<T>);
+    mgc.def("rational_approximation", &Metric::rational_approximation<T>);
+    mgc.def("normal_CDF_inverse", &Metric::normal_CDF_inverse<T>);
+    mgc.def("icdf_normal", &Metric::icdf_normal<T>);
+    mgc.def("significant_local_correlation", &Metric::significant_local_correlation<T>);
+    mgc.def("frobeniusNorm", &Metric::frobeniusNorm<T>);
+    mgc.def("max_in_matrix_regarding_second_boolean_matrix", &Metric::max_in_matrix_regarding_second_boolean_matrix<T>);
+    mgc.def("optimal_local_generalized_correlation", &Metric::optimal_local_generalized_correlation<T>);
+}
+
 void export_metric_MGC()
 {
+    // TODO: add more metrics
     wrap_metric_MGC<std::vector<double>, metric::Euclidian<double>, metric::Euclidian<double>>();
+    wrap_metric_MGC_direct<double>();
 }
