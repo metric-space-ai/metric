b2b21b9199cd1f4d41ec1cdbd1f0684c82ea9c38
jura-gresko <juragresko@gmail.com>
2020-03-23 23:44:06 -0300

    enable correlation module again, fix errors, for now we do copy

--- python/src/correlation/MGC.cpp
+++ python/src/correlation/MGC.cpp
@@ -7,59 +7,54 @@
 */
 
 #include "modules/correlation/mgc.hpp"
-#include "../stl_wrappers.hpp"
 #include "../metric_types.hpp"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 #include <string>
+#include <vector>
 
 class NotUsed {};
 
 namespace py = pybind11;
 
-template <class RecType, class Metric1, class Metric2>
+template <class ValueType, class Metric1, class Metric2>
 void wrap_metric_MGC(py::module& m) {
     using Metric = metric::MGC<NotUsed, Metric1, NotUsed, Metric2>;
-    using Container = std::vector<RecType>;
+    using Container = std::vector<std::vector<ValueType>>;
     std::string name = "MGC_" + getMetricName<Metric1>() + "_" + getMetricName<Metric2>();
     auto mgc = py::class_<Metric>(m, name.c_str(), "Multiscale Graph Correlation");
-    mgc.def("__call__", +[](Metric& self,
-        const WrapStlMatrix<double>& a,
-        const WrapStlMatrix<double>& b) {
-            return self(a, b);
-        },
-        (
-            py::arg("a"),
-            py::arg("b")
-        ),
-        "Return correlation betweeen a and b"
+    mgc.def(py::init<>(),
+        "Construct Multiscale Graph Correlation"
     );
-    mgc.def("estimate", +[](Metric& self,
-        const WrapStlMatrix<double>& a,
-        const WrapStlMatrix<double>& b,
-        size_t b_sample_size = 250,
-        double threshold = 0.05,
-        size_t max_iterations = 1000) {
-            return self.estimate(a, b, b_sample_size, threshold, max_iterations);
-        },
-        (
-            py::arg("a"),
-            py::arg("b"),
-            py::arg("b_sample_size") = 250,
-            py::arg("threshold") = 0.05,
-            py::arg("max_iterations") = 1000
-        ),
-        "Return estimate of the correlation between a and b"
+    mgc.def(py::init<const Metric1&, const Metric2&>(),
+        "Construct Multiscale Graph Correlation",
+        py::arg("m1"),
+        py::arg("m2")
     );
-    mgc.def("xcorr", +[](Metric& self,
-        const WrapStlMatrix<double>& a,
-        const WrapStlMatrix<double>& b,
-        int n) {
-            return self.xcorr(a, b, n);
-        }
+    // both methods means the same, but bind function much easier
+    mgc.def("__call__", &Metric::template operator()<Container, Container>,
+        "Return correlation betweeen a and b",
+        py::arg("a"),
+        py::arg("b")
+    );
+    mgc.def("xcorr", &Metric::template xcorr<Container, Container>,
+        "Return vector of mgc values calculated for different data shifts",
+        py::arg("a"),
+        py::arg("b"),
+        py::arg("n")
+    );
+    mgc.def("estimate", &Metric::template estimate<Container, Container>,
+        "Return estimate of the correlation between a and b",
+        py::arg("a"),
+        py::arg("b"),
+        py::arg("b_sample_size") = 250,
+        py::arg("threshold") = 0.05,
+        py::arg("max_iterations") = 1000
     );
 
-//    mgc.def("compute_distance_matrix", +[](Metric& self,  FIXME: Metric parameter undefined
+//    mgc.def("compute_distance_matrix", &Metric::template computeDistanceMatrix<Container, Metric+[](Metric& self,  FIXME: Metric parameter undefined
 //        const WrapStlMatrix<double>& c) {
 //            return self.computeDistanceMatrix(c);
 //        }
@@ -78,7 +73,7 @@ template <class T>
 void wrap_metric_MGC_direct(py::module& m) {
     using Metric = metric::MGC_direct;
     auto mgc = py::class_<Metric>(m, "MGC_direct");
-    mgc.def("__call__", &Metric::operator()<T>);   // FIXME: unsupported argument types
+    mgc.def("__call__", &Metric::xcorr<T>);   // FIXME: unsupported argument types
     mgc.def("xcorr", &Metric::xcorr<T>);           // FIXME: unsupported argument types
     mgc.def("center_distance_matrix", &Metric::center_distance_matrix<T>);
     mgc.def("rank_distance_matrix", &Metric::rank_distance_matrix<T>);
@@ -99,25 +94,25 @@ void export_metric_MGC(py::module& m)
 {
     using T = double;
     // TODO: loop
-    wrap_metric_MGC<std::vector<T>, metric::Euclidian<T>, metric::Euclidian<T>>(m);
-    wrap_metric_MGC<std::vector<T>, metric::Euclidian<T>, metric::Manhatten<T>>(m);
-//    wrap_metric_MGC<std::vector<T>, metric::Euclidian<T>, metric::Chebyshev<T>>(m);
-    wrap_metric_MGC<std::vector<T>, metric::Euclidian<T>, metric::P_norm<T>>(m);
+    wrap_metric_MGC<T, metric::Euclidian<T>, metric::Euclidian<T>>(m);
+    wrap_metric_MGC<T, metric::Euclidian<T>, metric::Manhatten<T>>(m);
+//    wrap_metric_MGC<T, metric::Euclidian<T>, metric::Chebyshev<T>>(m);
+    wrap_metric_MGC<T, metric::Euclidian<T>, metric::P_norm<T>>(m);
 
-    wrap_metric_MGC<std::vector<T>, metric::Manhatten<T>, metric::Euclidian<T>>(m);
-    wrap_metric_MGC<std::vector<T>, metric::Manhatten<T>, metric::Manhatten<T>>(m);
-//    wrap_metric_MGC<std::vector<T>, metric::Manhatten<T>, metric::Chebyshev<T>>(m);
-    wrap_metric_MGC<std::vector<T>, metric::Manhatten<T>, metric::P_norm<T>>(m);
+    wrap_metric_MGC<T, metric::Manhatten<T>, metric::Euclidian<T>>(m);
+    wrap_metric_MGC<T, metric::Manhatten<T>, metric::Manhatten<T>>(m);
+//    wrap_metric_MGC<T, metric::Manhatten<T>, metric::Chebyshev<T>>(m);
+    wrap_metric_MGC<T, metric::Manhatten<T>, metric::P_norm<T>>(m);
 
-//    wrap_metric_MGC<std::vector<T>, metric::Chebyshev<T>, metric::Euclidian<T>>();
-//    wrap_metric_MGC<std::vector<T>, metric::Chebyshev<T>, metric::Manhatten<T>>();
-//    wrap_metric_MGC<std::vector<T>, metric::Chebyshev<T>, metric::Chebyshev<T>>();
-//    wrap_metric_MGC<std::vector<T>, metric::Chebyshev<T>, metric::P_norm<T>>();
+//    wrap_metric_MGC<T, metric::Chebyshev<T>, metric::Euclidian<T>>();
+//    wrap_metric_MGC<T, metric::Chebyshev<T>, metric::Manhatten<T>>();
+//    wrap_metric_MGC<T, metric::Chebyshev<T>, metric::Chebyshev<T>>();
+//    wrap_metric_MGC<T, metric::Chebyshev<T>, metric::P_norm<T>>();
 
-    wrap_metric_MGC<std::vector<T>, metric::P_norm<T>, metric::Euclidian<T>>(m);
-    wrap_metric_MGC<std::vector<T>, metric::P_norm<T>, metric::Manhatten<T>>(m);
-//    wrap_metric_MGC<std::vector<T>, metric::P_norm<T>, metric::Chebyshev<T>>();
-    wrap_metric_MGC<std::vector<T>, metric::P_norm<T>, metric::P_norm<T>>(m);
+    wrap_metric_MGC<T, metric::P_norm<T>, metric::Euclidian<T>>(m);
+    wrap_metric_MGC<T, metric::P_norm<T>, metric::Manhatten<T>>(m);
+//    wrap_metric_MGC<T, metric::P_norm<T>, metric::Chebyshev<T>>();
+    wrap_metric_MGC<T, metric::P_norm<T>, metric::P_norm<T>>(m);
 
     wrap_metric_MGC_direct<T>(m);
 }
