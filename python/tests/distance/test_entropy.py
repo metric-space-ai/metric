80ced3b67cdfd721d56217bfe978e6076bfd1204
jura-gresko <juragresko@gmail.com>
2020-03-25 17:37:22 -0300

    update entropy test

--- python/tests/distance/test_entropy.py
+++ python/tests/distance/test_entropy.py
@@ -1,12 +1,21 @@
-from metric.distance import entropy, Euclidean, Manhatten
+from metric.distance import entropy, Euclidean, Manhatten, Chebyshev, P_norm
 import numpy
+import math
+
+
+def my_euclidean(a, b):
+    result = math.sqrt(sum(((x - y) * (x - y) for x, y in zip(a, b))))
+    print('python', result)
+    return result
 
 
 def test_entropy():
     v = numpy.float_([[5, 5], [2, 2], [3, 3], [5, 1]])
 
-    assert entropy(v) == 7.819791963526808
-    assert entropy(v, metric='euclidean') == 7.819791963526808
-    assert entropy(v, metric='manhatten') == 7.918295834054489
-    assert entropy(v, metric='chebyshev') == 7.625814583693912
-    assert entropy(v, metric='p-norm') == 7.918295834054489
+    assert entropy(v) == 5.967356977156464
+    assert entropy(v, metric=Euclidean()) == 5.967356977156464
+    assert entropy(v, metric=Manhatten()) == 6.065860847684145
+    assert entropy(v, metric=Chebyshev()) == 5.773379597323566
+    assert entropy(v, metric=P_norm()) == 6.065860847684145
+    assert entropy(v, metric=P_norm(p=10)) == 5.852646926226563
+    assert entropy(v, metric=my_euclidean) == 5.315860847684144  # different value because of hardcoded Euclidean (p)
