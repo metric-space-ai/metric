cf67788351f94ae6a3f54d1eba3cb70d2e074253
Dmitry Vinokurov <dmitry.vinokurov@gmail.com>
2020-03-28 23:34:26 +0600

    add insert and element access operations to KNNGraph, issue #37

--- modules/utils/graph.hpp
+++ modules/utils/graph.hpp
@@ -342,7 +342,7 @@ public:
      * @brief Construct a new KNN Graph object
      * 
      */
-    KNNGraph(std::vector<Sample> X, size_t neighbors_num, size_t max_bruteforce_size, int max_iterations = 100, double update_range = 0.02);
+    KNNGraph(const std::vector<Sample>& X, size_t neighbors_num, size_t max_bruteforce_size, int max_iterations = 100, double update_range = 0.02);
 
     /**
      * @brief Construct a new KNN Graph object
@@ -362,57 +362,112 @@ public:
     // */
     //KNNGraph(std::vector<std::vector<typename Distance::value_type>> distance_matrix);
 
-	
-	size_t neighbors_num() { return _neighbors_num; };
 
-	
-	Sample get_node_data(size_t i) { return _nodes[i]; };
+    size_t neighbors_num() { return _neighbors_num; };
 
-	
-	std::vector<int> gnnn_search(Sample query, int max_closest_num, int iterations = 10, int num_greedy_moves = -1, int num_expansions = -1);
 
+    Sample get_node_data(size_t i) { return _nodes[i]; };
+
+    std::vector<int> gnnn_search(const Sample & query, int max_closest_num, int iterations = 10, int num_greedy_moves = -1, int num_expansions = -1);
+
+    /**
+     * @brief return value with index equals idx
+     * @param idx index of searching value
+     */
+    const Sample & operator[](std::size_t idx) const { return _nodes[idx]; }
+
+    /**
+     * @brief return distance between elements with indexes i and j
+     * @param i index of the first element
+     * @param j index of the second element
+     */
+    typename Distance::distance_type operator()(std::size_t i, std::size_t j) const {
+        return _distance_matrix[i][j];
+    }
+
+    /**
+     * @brief insert new element into the graph
+     * @param p new element
+     * return index of the inserted element
+     */
+    std::size_t insert(const Sample & p);
+    /**
+     * @brief insert set of the new elements into the graph
+     * @param p - container with new elements
+     * @return vector of indexes of the new elements
+     */
+    std::vector<std::size_t> insert(const std::vector<Sample>& p);
+
+    /**
+     * @brief insert new element into the graph if distance between this element and its NN is greater than threshold
+     * @param p new element
+     * @param threshold distance threshold
+     * @return pair consist of index and result of insertion
+     */
+    std::pair<std::size_t, bool> insert_if(const Sample& p, typename Distance::distance_type threshold);
+
+    /**
+     * @brief append data records into the Matrix only if distance bigger than a treshold
+     *
+     * @param items set of new records
+     * @param treshold distance threshold
+     * @return vector of pairs consists of indexes and results of insertion
+     */
+    std::vector<std::pair<std::size_t,bool>> insert_if(const std::vector<Sample>& items, typename Distance::distance_type threshold);
+
+    /**
+     * @brief erase element from graph
+     * @param idx index of erasing element
+     */
+    void erase(std::size_t idx);
+
+    /**
+     * @brief return size of graph
+     *
+     */
+    std::size_t size() const { return _nodes.size(); }
+    
 protected:
-	size_t _neighbors_num = 1;
-	size_t _max_bruteforce_size = 10;
-	
-	int _max_iterations = 100;
-	double _update_range = 0.02;
+    size_t _neighbors_num = 1;
+    size_t _max_bruteforce_size = 10;
 
-	bool _not_more_neighbors = false;
+    int _max_iterations = 100;
+    double _update_range = 0.02;
 
-	std::vector<Sample> _nodes;
-	std::vector<std::vector<typename Distance::value_type>> _distance_matrix;
+    bool _not_more_neighbors = false;
 
+    std::vector<Sample> _nodes;
+    std::vector<std::vector<typename Distance::value_type>> _distance_matrix;
 private:
     /**
      * @brief 
      * 
      */
-    void construct(std::vector<Sample> X);
+    void construct(const std::vector<Sample> &X);
 
     /**
      * @brief 
      * 
      */
-    void calculate_distance_matrix(std::vector<Sample> X);
+    void calculate_distance_matrix(const std::vector<Sample> &X);
 
     /**
      * @brief 
      * 
      */
-    void make_edge_pairs(std::vector<Sample> X);
+    void make_edge_pairs(const std::vector<Sample> &X);
 	
     /**
      * @brief 
      * 
      */
-	std::vector<std::pair<size_t, size_t>> brute_force(std::vector<Sample> samples, std::vector<int> ids);
+	std::vector<std::pair<size_t, size_t>> brute_force(const std::vector<Sample> &samples, const std::vector<int> &ids);
 	
     /**
      * @brief 
      * 
      */
-	std::vector<std::pair<size_t, size_t>> random_pair_division(std::vector<Sample> samples, std::vector<int> ids, int max_size);
+	std::vector<std::pair<size_t, size_t>> random_pair_division(const std::vector<Sample> &samples, const std::vector<int> &ids, int max_size);
 
     /**
      * @brief 
