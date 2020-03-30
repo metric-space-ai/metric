d3db20ca5ed92c42bb14c92642298a46599f51b4
jura-gresko <juragresko@gmail.com>
2020-03-30 13:01:05 -0300

    fix bad merge with master, + update entropy test to pass

---
+++ python/pkg/metric/transform/__init__.py
@@ -0,0 +1,1 @@
+from metric._impl.transform import *
--- python/tests/correlation/test_entropy.py
+++ python/tests/correlation/test_entropy.py
@@ -13,10 +13,10 @@ def test_entropy():
     v = numpy.float_([[5, 5], [2, 2], [3, 3], [5, 1]])
     entropy = partial(Entropy, p=3, k=2)
 
-    assert entropy()(v) == 5.967356977156464
-    assert entropy(metric=Euclidean())(v) == 5.967356977156464
-    assert entropy(metric=Manhatten())(v) == 6.065860847684145
-    assert entropy(metric=Chebyshev())(v) == 5.773379597323566
-    assert entropy(metric=P_norm())(v) == 6.065860847684145
-    assert entropy(metric=P_norm(p=10))(v) == 5.852646926226563
-    assert entropy(metric=my_euclidean)(v) == 5.315860847684144  # different value because of hardcoded Euclidean (p)
+    assert entropy()(v) == 1.014045451620507
+    assert entropy(metric=Euclidean())(v) == 1.014045451620507
+    assert entropy(metric=Manhatten())(v) == 1.582185490039048
+    assert entropy(metric=Chebyshev())(v) == -3.9489104772539405
+    assert entropy(metric=P_norm())(v) == 1.582185490039048
+    assert entropy(metric=P_norm(p=10))(v) == -1.6957132689132957
+    assert entropy(metric=my_euclidean)(v) == 1.014045451620507  # different value because of hardcoded Euclidean (p)
