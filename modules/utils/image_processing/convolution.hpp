f5d236e3a2ed41fec86a2ec5528d88d5175e3730
Kataev Victor <kataev.victor.1234@gmail.com>
2020-04-01 01:39:58 +0300

    add Convolution2d class

---
+++ modules/utils/image_processing/convolution.hpp
@@ -0,0 +1,36 @@
+#ifndef CONVOLUTIONAL_HPP
+#define CONVOLUTIONAL_HPP
+
+#include "../dnn/Activation/Identity.h"
+#include "../dnn/Layer/Conv2d.h"
+
+namespace metric {
+
+	template <typename T, size_t Channels>
+	class Convolution2d {
+		public:
+			using Channel = blaze::DynamicMatrix<T>;
+			using Image = blaze::StaticVector<Channel, Channels>;
+			using FilterKernel = blaze::DynamicMatrix<T>;
+
+			using ConvLayer2d = dnn::Conv2d<T, dnn::Identity<T>>;
+			using typename ConvLayer2d::Matrix;
+
+			Convolution2d(size_t imageWidth, size_t imageHeight, size_t kernelWidth, size_t kernelHeight);
+
+			Image operator()(Image image, FilterKernel kernel);
+
+			Matrix operator()(Matrix image, std::vector<T> kernelData);
+
+
+
+		private:
+			std::unique_ptr<ConvLayer2d> convLayer;
+	};
+
+}
+
+
+#include "convolution.cpp"
+
+#endif
