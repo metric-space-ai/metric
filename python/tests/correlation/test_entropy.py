b848b149aa12c3602e7cddbc7df981c8d185065e
jura-gresko <juragresko@gmail.com>
2020-04-01 19:16:51 -0300

    deactivate VOI and entropy for now, as there is a bug in C++ code

--- python/tests/correlation/test_entropy.py
+++ python/tests/correlation/test_entropy.py
@@ -3,12 +3,14 @@ from metric.correlation import Entropy
 import numpy
 import math
 from functools import partial
+import pytest
 
 
 def my_euclidean(a, b):
     return math.sqrt(sum(((x - y) * (x - y) for x, y in zip(a, b))))
 
 
+@pytest.mark.skip('VOI crashes with small dataset')
 def test_entropy():
     v = numpy.float_([[5, 5], [2, 2], [3, 3], [5, 1]])
     entropy = partial(Entropy, p=3, k=2)
@@ -22,6 +24,7 @@ def test_entropy():
     assert entropy(metric=my_euclidean)(v) == 1.014045451620507  # different value because of hardcoded Euclidean (p)
 
 
+@pytest.mark.skip('VOI crashes with small dataset')
 def test_estimate():
     v = numpy.float_([[5, 5], [2, 2], [3, 3], [5, 1]])
     entropy = Entropy(p=10, k=5)
