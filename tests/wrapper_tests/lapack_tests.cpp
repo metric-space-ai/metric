bfc23f5611152f6792a5a78c8639a2dae549fec5
Kataev Victor <kataev.victor.1234@gmail.com>
2020-04-26 23:35:32 +0300

    add blaze wrapper

--- tests/wrapper_tests/lapack_tests.cpp
+++ tests/wrapper_tests/lapack_tests.cpp
@@ -14,7 +14,27 @@ using Matrix = blaze::DynamicMatrix<double>;
 using Vector = blaze::DynamicVector<double>;
 
 
-BOOST_AUTO_TEST_CASE(fullyconnected_json)
+BOOST_AUTO_TEST_CASE(sygv_c)
 {
+    std::vector<double> A = { 1, 2, 2, 4 };
+    std::vector<double> B = { 5, 6, 6, 8 };
 
+    std::vector<double> W(2);
+    std::vector<double> work(5);
+
+    int info;
+    dsygv(1, 'N', 'U', 2, A.data(), 2, B.data(), 2, W.data(), work.data(), 5, info);
+
+    std::cout << W[0] << " " << W[1] << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(sygv_blaze)
+{
+    Matrix A{{1, 2}, {2, 4}};
+    Matrix B{{5, 6}, {6, 8}};
+    Vector w{0, 0};
+
+    sygv<double, double>(A, B, w);
+
+    std::cout << w[0] << " " << w[1] << std::endl;
 }
