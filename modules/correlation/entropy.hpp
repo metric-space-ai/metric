7afb45f98bf67a3ee594623e078ccab9c18ca988
maxfilippov <maxfilippov@aol.com>
2020-04-27 16:30:29 +0300

    updated Tree, updating Entropy

---
+++ modules/correlation/entropy.hpp
@@ -0,0 +1,55 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2020 Panda Team
+*/
+
+// TODO complete refactoring!!
+
+#ifndef _METRIC_CORRELATION_ENTROPY_HPP
+#define _METRIC_CORRELATION_ENTROPY_HPP
+
+#include "../distance/k-related/Standards.hpp"
+
+#include "../../3rdparty/blaze/Blaze.h"
+
+
+namespace metric {
+
+
+
+template <typename MT = metric::Chebyshev<double>>
+class Entropy {
+public:
+    Entropy(Metric metric = MT(), size_t k = 7, size_t p = 70, bool exp = false) :
+        metric(metric),
+        k(k),
+        p(p),
+        exp(exp) {}
+
+    template <typename Container>
+    double operator()(const Container& data) const;
+
+    template <typename Container>
+    double estimate(
+            const Container & a,
+            const size_t sampleSize = 250,
+            const double threshold = 0.05,
+            size_t maxIterations = 1000
+    ) const;
+
+private:
+    size_t k;
+    size_t p;
+    Metric metric;
+    bool exp;
+};
+
+
+
+} // namespace metric
+
+//#include "entropy.cpp"
+#endif
---
+++ modules/space.hpp
@@ -0,0 +1,27 @@
+/*
+                       
+PANDA presents
+
+███╗   ███╗███████╗████████╗██████╗ ██╗ ██████╗    ███████╗██████╗  █████╗  ██████╗███████╗
+████╗ ████║██╔════╝╚══██╔══╝██╔══██╗██║██╔════╝    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝
+██╔████╔██║█████╗     ██║   ██████╔╝██║██║         ███████╗██████╔╝███████║██║     █████╗  
+██║╚██╔╝██║██╔══╝     ██║   ██╔══██╗██║██║         ╚════██║██╔═══╝ ██╔══██║██║     ██╔══╝  
+██║ ╚═╝ ██║███████╗   ██║   ██║  ██║██║╚██████╗    ███████║██║     ██║  ██║╚██████╗███████╗
+╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝    ╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝
+                                                                        Licensed under MPL 2.0. 
+                                                                                                   
+a library for metric space data containers and corresponding basic methods
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) PANDA Team
+*/
+
+#ifndef _METRIC_SPACE_HPP
+#define _METRIC_SPACE_HPP
+
+#include "space/tree.hpp"
+
+#endif
