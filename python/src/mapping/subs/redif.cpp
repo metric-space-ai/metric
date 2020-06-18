622334814415bd7a5d61b7f00e07e763b79eba9b
stipan <micromillioner@yahoo.com>
2020-06-18 17:19:25 +0200

    [redif] python fix

--- python/src/mapping/subs/redif.cpp
+++ python/src/mapping/subs/redif.cpp
@@ -19,15 +19,10 @@ void register_wrapper_Redif(py::module& m) {
         py::arg("n_iter") = 15,
         py::arg("metric") = Metric()
     );
-    std::tuple<Container, std::vector<size_t>> (Class::*encode_vector)(const Container&) = &Class::encode;
-    cls.def("encode", encode_vector,
-        py::arg("x")
-	);
-    Container (Class::*decode_vector)(const Container&, const std::vector<size_t>&) = &Class::decode;
-    cls.def("decode", decode_vector,
-        py::arg("xEncoded"),
-        py::arg("l_idx")
-	);
+    Container (Class::*encode_vector)(const Container&) = &Class::encode;
+    cls.def("encode", encode_vector, py::arg("x"));
+    Container (Class::*decode_vector)(const Container&) = &Class::decode;
+    cls.def("decode", decode_vector, py::arg("xEncoded"));
 }
 
 void export_metric_Redif(py::module& m) {
