411741d1b913dbba886f056353fb5f077b19ca61
maxfilippov <maxfilippov@aol.com>
2020-07-07 14:14:06 +0300

    added example for D4 Daubechies transform matrix generator

--- examples/transform_examples/daubechies_mat_example.cpp
+++ examples/transform_examples/daubechies_mat_example.cpp
@@ -8,9 +8,16 @@
 
 int main() {
 
-    auto db4 = wavelet::DaubechiesMat<double>(14);
+    blaze::DynamicVector<double> v {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    //blaze::DynamicVector<double> v {0, 0.2, 0.4, 0.6, 0.8, 1};
 
+    auto db4 = wavelet::DaubechiesMat<double>(14);
     std::cout << db4 << "\n";
+
+    blaze::DynamicVector<double> encoded = db4*v; // dont use auto here, it will lead to matrix, whereas we need column vector
+    std::cout << encoded << "\n";
+    auto decoded = db4.transpose()*encoded;
+    std::cout << decoded << "\n";
 
     return 0;
 }
