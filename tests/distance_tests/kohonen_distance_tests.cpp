9528a5758ced2b70ce0a728ed59e184b6435592d
Kataev Victor <kataev.victor.1234@gmail.com>
2021-02-12 01:40:21 +0300

    move kohoren_distance to Catch2

--- tests/distance_tests/kohonen_distance_tests.cpp
+++ tests/distance_tests/kohonen_distance_tests.cpp
@@ -6,170 +6,85 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Copyright (c) 2019 Panda Team
 */
 
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
 #include <vector>
 #include <iostream>
-#include <fstream>
 
 #include <chrono>
 #include "../../modules/distance.hpp"
 
 
-std::tuple<std::vector<std::vector<double>>, std::vector<double>> readData(std::string filename)
+TEMPLATE_TEST_CASE("kohonen", "[distance]", float, double)
 {
-	std::fstream fin;
-
-	fin.open(filename, std::ios::in);
-	
-	std::vector<double> row;
-	std::string line, word, w;
-
-	std::vector<std::vector<double>> rows;
-	std::vector<double> labels;
-
-	// omit headers 
-	//getline(fin, line);
-
-	while (getline(fin, line))
-	{
-		//std::cout << "row " << i << std::endl;
-		std::stringstream s(line);
-		//std::cout << " -> " << line << std::endl;
-
-		row.clear();
-		int i = 0;
-		while (getline(s, word, '\t'))
-		{
-			//std::cout << " --> " << word << std::endl;
-			
-			if (i >= 2)
-			{
-				labels.push_back(std::atof(word.c_str()));
-			}
-			else
-			{
-				row.push_back(std::atof(word.c_str()));
-			}
-			i++;
-		}
-
-		rows.push_back(row);
-	}
-
-	return { rows, labels };
-}
-
-
-int main()
-{
-	/******************** examples for Kohonen Distance **************************/
-
-	std::cout << "Kohonen Distance example have started" << std::endl;
-	std::cout << "" << std::endl;
-
 	size_t grid_w = 3;
 	size_t grid_h = 2;
 	
-	using Record = std::vector<double>;
+	using Record = std::vector<TestType>;
 		
 	std::vector<Record> train_dataset = {
 		{0, 0},
 		{1, 0},
 		{2, 0},
-
 		{0, 1},
 		{1, 1},
 		{2, 1},
 	};
 
-	double result;
 
+	metric::Kohonen<TestType, Record> distance_1(train_dataset, grid_w, grid_h);
 
-	///
-	
-	metric::Kohonen<double, Record> distance_1(train_dataset, grid_w, grid_h);
-
-	for (int i = 1; i < 6; i++)
-	{
-		result = distance_1(train_dataset[0], train_dataset[i]);
-		std::cout << "result: " << result << std::endl;
-	}
-	std::cout << "" << std::endl;
+	TestType result;
 	result = distance_1(train_dataset[0], train_dataset[1]);
-	assert(result > 0 && result < 2);
+	REQUIRE(result > 0); REQUIRE(result < 2);
 	result = distance_1(train_dataset[0], train_dataset[2]);
-	assert(result > 1 && result < 3);
+	REQUIRE(result > 1); REQUIRE(result < 3);
 	result = distance_1(train_dataset[0], train_dataset[3]);
-	assert(result > 0 && result < 2);
+	REQUIRE(result > 0); REQUIRE(result < 2);
 	result = distance_1(train_dataset[0], train_dataset[4]);
-	assert(result > 1 && result < 3);
+	REQUIRE(result > 1); REQUIRE(result < 3);
 	result = distance_1(train_dataset[0], train_dataset[5]);
-	assert(result > 2 && result < 4);
+	REQUIRE(result > 2); REQUIRE(result < 4);
 	
 
-	///
-	
-    using Vector = std::vector<double>;
-    using Metric = metric::Euclidean<double>;
+    using Vector = std::vector<TestType>;
+    using Metric = metric::Euclidean<TestType>;
     using Graph = metric::Grid6; 
-    using Distribution = std::uniform_real_distribution<double>; 
+    using Distribution = std::uniform_real_distribution<TestType>;
 
 	Distribution distr(-1, 1);
 
     metric::SOM<Vector, Graph, Metric> som_model(Graph(grid_w, grid_h), Metric(), 0.8, 0.2, 50, distr);
-	
-    if (!som_model.isValid()) {
-    	std::cout << "SOM is not valid" << std::endl;
-    	return EXIT_FAILURE;
-    }
 
 	som_model.train(train_dataset);
 	
-	metric::Kohonen<double, Vector, Graph, Metric> distance_2(som_model, train_dataset);
-	
-	for (int i = 1; i < 6; i++)
-	{
-		result = distance_2(train_dataset[0], train_dataset[i]);
-		std::cout << "result: " << result << std::endl;
-	}
-	std::cout << "" << std::endl;
-	result = distance_2(train_dataset[0], train_dataset[1]);
-	assert(result > 0 && result < 2);
-	result = distance_2(train_dataset[0], train_dataset[2]);
-	assert(result > 1 && result < 3);
-	result = distance_2(train_dataset[0], train_dataset[3]);
-	assert(result > 0 && result < 2);
-	result = distance_2(train_dataset[0], train_dataset[4]);
-	assert(result > 1 && result < 3);
-	result = distance_2(train_dataset[0], train_dataset[5]);
-	assert(result > 2 && result < 4);
-	
+	metric::Kohonen<TestType, Vector, Graph, Metric> distance_2(som_model, train_dataset);
 
-	///
-	// negative values
+	result = distance_2(train_dataset[0], train_dataset[1]);
+	REQUIRE(result > 0); REQUIRE(result < 2);
+	result = distance_2(train_dataset[0], train_dataset[2]);
+	REQUIRE(result > 1); REQUIRE(result < 3);
+	result = distance_2(train_dataset[0], train_dataset[3]);
+	REQUIRE(result > 0); REQUIRE(result < 2);
+	result = distance_2(train_dataset[0], train_dataset[4]);
+	REQUIRE(result > 1); REQUIRE(result < 3);
+	result = distance_2(train_dataset[0], train_dataset[5]);
+	REQUIRE(result > 2); REQUIRE(result < 4);
 	
     metric::SOM<Vector, Graph, Metric> negative_som_model(Graph(grid_w, grid_h), Metric(), -0.8, -0.2, 50, distr);
 	negative_som_model.train(train_dataset);
 	
-	metric::Kohonen<double, Vector, Graph, Metric> distance_3(negative_som_model, train_dataset);
-	
-	for (int i = 1; i < 6; i++)
-	{
-		result = distance_3(train_dataset[0], train_dataset[i]);
-		std::cout << "result: " << result << std::endl;
-	}
-	std::cout << "" << std::endl;
+	metric::Kohonen<TestType, Vector, Graph, Metric> distance_3(negative_som_model, train_dataset);
+
 	result = distance_3(train_dataset[0], train_dataset[1]);
-	assert(result > 0 && result < 2);
+	REQUIRE(result > 0); REQUIRE(result < 2);
 	result = distance_3(train_dataset[0], train_dataset[2]);
-	assert(result > 1 && result < 3);
+	REQUIRE(result > 1); REQUIRE(result < 3);
 	result = distance_3(train_dataset[0], train_dataset[3]);
-	assert(result > 0 && result < 2);
+	REQUIRE(result > 0); REQUIRE(result < 2);
 	result = distance_3(train_dataset[0], train_dataset[4]);
-	assert(result > 1 && result < 3);
+	REQUIRE(result > 1); REQUIRE(result < 3);
 	result = distance_3(train_dataset[0], train_dataset[5]);
-	assert(result > 2 && result < 4);
-
-	
-
-	return 0;
+	REQUIRE(result > 2); REQUIRE(result < 4);
 }
