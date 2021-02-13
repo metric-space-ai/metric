c96e4e558be9c3d34d2e5f6c45a46e63cea52bdd
Kataev Victor <kataev.victor.1234@gmail.com>
2021-02-13 14:40:53 +0300

    move lapack test to catch2

--- tests/wrapper_tests/lapack_tests.cpp
+++ tests/wrapper_tests/lapack_tests.cpp
@@ -1,12 +1,11 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MAIN
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-#include <boost/test/unit_test.hpp>
 #include <limits>
 
-#include <iostream>
-//#include "modules/utils/wrappers/lapack.hpp"
 #include "modules/distance/d-spaced/Riemannian.hpp"
+
+#include <iostream>
 
 
 using namespace metric;
@@ -15,7 +14,7 @@ using Matrix = blaze::DynamicMatrix<double>;
 using Vector = blaze::DynamicVector<double>;
 
 
-BOOST_AUTO_TEST_CASE(sygv_c)
+TEST_CASE("sygv_c")
 {
     std::vector<double> A = { 1, 2, 2, 4 };
     std::vector<double> B = { 5, 6, 6, 8 };
@@ -26,10 +25,10 @@ BOOST_AUTO_TEST_CASE(sygv_c)
     int info;
     dsygv(1, 'N', 'U', 2, A.data(), 2, B.data(), 2, W.data(), work.data(), 5, info);
 
-    std::cout << W[0] << " " << W[1] << std::endl;
+    //std::cout << W[0] << " " << W[1] << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(sygv_blaze)
+TEST_CASE("sygv_blaze")
 {
     Matrix A{{1, 2}, {2, 4}};
     Matrix B{{5, 6}, {6, 8}};
@@ -37,10 +36,10 @@ BOOST_AUTO_TEST_CASE(sygv_blaze)
 
     sygv<double, double>(A, B, w);
 
-    std::cout << w[0] << " " << w[1] << std::endl;
+    //std::cout << w[0] << " " << w[1] << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(riemannian_distance)
+TEST_CASE("riemannian_distance")
 {
     Matrix A{{1, 2}, {2, 4}};
     Matrix B{{5, 6}, {6, 8}};
@@ -51,6 +50,6 @@ BOOST_AUTO_TEST_CASE(riemannian_distance)
     std::vector<std::vector<double>> ds1 {{0, 1}, {0, 0}, {1, 1}, {1, 0}};
     std::vector<std::vector<double>> ds2 {{0, 0}, {1, 1}, {2, 2}, {2, 1}};
 
-    std::cout << rd(ds1, ds2) << "\n";
-    std::cout << rd.estimate(ds1, ds2) << "\n";
+    rd(ds1, ds2);
+    rd.estimate(ds1, ds2);
 }
