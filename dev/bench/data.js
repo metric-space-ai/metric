6d498b492d0326e6e04e9a41d0069d1d151bf265
github-action-benchmark <github@users.noreply.github.com>
2021-03-07 20:58:16 +0000

    add Convolution (image=[1920x1080], kerne=5) (catch2) benchmark result for d8951da656c80b4032140d5fba1fa869f62abca0

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1615150696387,
+  "repoUrl": "https://github.com/panda-official/metric",
+  "entries": {
+    "Convolution (image=[1920x1080], kerne=5)": [
+      {
+        "commit": {
+          "author": {
+            "name": "panda-official",
+            "username": "panda-official"
+          },
+          "committer": {
+            "name": "panda-official",
+            "username": "panda-official"
+          },
+          "id": "d8951da656c80b4032140d5fba1fa869f62abca0",
+          "message": "init CB",
+          "timestamp": "2021-03-06T09:40:48Z",
+          "url": "https://github.com/panda-official/metric/pull/263/commits/d8951da656c80b4032140d5fba1fa869f62abca0"
+        },
+        "date": 1615150695617,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Convolution2d.operator()",
+            "value": 128.022,
+            "range": "± 2.06769",
+            "unit": "ms",
+            "extra": "3 samples\n1 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
