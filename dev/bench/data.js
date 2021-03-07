a2477cd21d2e1d943a31ac1c763ab57c4e26e36f
Victor <kataev.victor.1234@gmail.com>
2021-03-08 01:01:29 +0300

    add first run

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -13,6 +13,33 @@ window.BENCHMARK_DATA = {
             "name": "panda-official",
             "username": "panda-official"
           },
+          "id": "d8951da656c80b4032140d5fba1fa869f62abca0",
+          "message": "init CB",
+          "timestamp": "2021-03-06T09:40:48Z",
+          "url": "https://github.com/panda-official/metric/pull/263/commits/d8951da656c80b4032140d5fba1fa869f62abca0"
+        },
+        "date": 1615150695617,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Convolution2d.operator()",
+            "value": 128.022,
+            "range": "± 2.06769",
+            "unit": "ms",
+            "extra": "3 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "panda-official",
+            "username": "panda-official"
+          },
+          "committer": {
+            "name": "panda-official",
+            "username": "panda-official"
+          },
           "id": "4f9b5a87bcd24821f58ec6b6418008d5b5489281",
           "message": "init CB",
           "timestamp": "2021-03-06T09:40:48Z",
