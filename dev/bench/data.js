98f47d6ce3301ee063df1bdf1b9097db1d7162ab
github-action-benchmark <github@users.noreply.github.com>
2021-03-07 21:52:57 +0000

    add Convolution (image=[1920x1080], kernel=5) (catch2) benchmark result for db227bc84ed525aa9cf9c565c8ec2a6ec51d0d49

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615153228161,
+  "lastUpdate": 1615153977312,
   "repoUrl": "https://github.com/panda-official/metric",
   "entries": {
     "Convolution (image=[1920x1080], kerne=5)": [
@@ -56,6 +56,37 @@ window.BENCHMARK_DATA = {
             "range": "± 1.04986",
             "unit": "ms",
             "extra": "3 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kataev.victor.1234@gmail.com",
+            "name": "Victor",
+            "username": "victor1234"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db227bc84ed525aa9cf9c565c8ec2a6ec51d0d49",
+          "message": "init CB (#263)\n\n* init CB\r\n\r\n* fix layout\r\n\r\n* fix workflow name, secret name\r\n\r\n* change benchmark name, remove cache\r\n\r\n* remove run om pull request, remove benchmark run number limit",
+          "timestamp": "2021-03-08T00:49:50+03:00",
+          "tree_id": "661518e87da16d2fde7c2c25e97dde62c1e9c5dc",
+          "url": "https://github.com/panda-official/metric/commit/db227bc84ed525aa9cf9c565c8ec2a6ec51d0d49"
+        },
+        "date": 1615153976398,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Convolution2d.operator()",
+            "value": 127.867,
+            "range": "± 2.59575",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
           }
         ]
       }
