e034284742cc07d7304e9d27cc7a50deabe99ac3
maxfilippov <maxfilippov@aol.com>
2021-04-28 11:18:44 +0300

    fixed path in load example

--- examples/mapping_examples/continious_mappers/classification/switch_est_load_example.cpp
+++ examples/mapping_examples/continious_mappers/classification/switch_est_load_example.cpp
@@ -100,8 +100,8 @@ int main()
 
     auto start_time = std::chrono::steady_clock::now();
 
-    //auto model = SwitchPredictor<value_type>("model.blaze");
-    auto model = SwitchPredictor<value_type>("esn_image.blaze");
+    auto model = SwitchPredictor<value_type>("model.blaze");
+    //auto model = SwitchPredictor<value_type>("esn_image.blaze");
 
     auto end_time = std::chrono::steady_clock::now();
     std::cout << "model loaded in " <<
