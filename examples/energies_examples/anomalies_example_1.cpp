0f0abe8cfe0067d5e65be8c226d4588b79a8676a
maxfilippov <maxfilippov@aol.com>
2021-07-24 19:17:38 +0300

    trying training

---
+++ examples/energies_examples/anomalies_example_1.cpp
@@ -0,0 +1,526 @@
+
+#include "modules/mapping/SOM.hpp"
+#include "modules/mapping/kmeans.hpp"
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+
+
+
+// ----- csv reader
+
+
+template <typename T> T convert_to(const std::string & str)
+{
+    std::istringstream s(str);
+    T num;
+    s >> num;
+    return num;
+} // templated version of stof, stod, etc., thanks to https://gist.github.com/mark-d-holmberg/862733
+
+
+template <class ValueType>
+std::vector<std::vector<ValueType>> read_csv_num(std::string filename, std::string sep=",")
+{
+    typedef typename std::vector<ValueType> LINE;
+    std::string line;
+    int pos;
+    std::vector<std::vector<ValueType>> array = {};
+    std::ifstream in(filename);
+    if(!in.is_open()) {
+        std::cout << "Failed to open file" << std::endl;
+        return array;
+    }
+    while( getline(in,line) ) {
+        LINE ln;
+        while( (pos = line.find(sep)) >= 0) {
+            std::string field = line.substr(0, pos);
+            line = line.substr(pos+1);
+            ln.push_back(convert_to<ValueType>(field));
+        }
+        ln.push_back(convert_to<ValueType>(line));
+        array.push_back(ln);
+    }
+    return array;
+}
+
+
+
+
+
+// ------ helper functions (copied from energies_example.cpp)
+
+namespace ooc_functions
+{
+
+
+template <typename T>
+std::vector<std::vector<T>>
+transpose(std::vector<std::vector<T>> &a)
+{
+
+  size_t rows = a.size();
+  size_t cols = a[0].size();
+
+  std::vector<std::vector<T>> array(cols, std::vector<T>(rows));
+  for (size_t i = 0; i < cols; ++i)
+  {
+    for (size_t j = 0; j < rows; ++j)
+    {
+      array[i][j] = a[j][i];
+    }
+  }
+  return array;
+}
+
+
+////
+
+
+
+// linspace (erzeugt einen linearen Datenvektor)
+template <typename T>
+std::vector<T>
+linspace(T a, T b, int n)
+{
+  std::vector<T> array;
+  if (n > 1)
+  {
+    T step = (b - a) / T(n - 1);
+    int count = 0;
+    while (count < n)
+    {
+      array.push_back(a + count * step);
+      ++count;
+    }
+  }
+  else
+  {
+    array.push_back(b);
+  }
+  return array;
+}
+
+template <typename T>
+T Lerp(T v0, T v1, T t)
+{
+  return (1 - t) * v0 + t * v1;
+}
+
+template <typename T>
+T quickQuantil(std::vector<T> data, T probs)
+{
+
+  if (!(data.size() > 0))
+    return 0;
+
+  if (1 == data.size())
+    return data[0];
+
+  T poi = Lerp(T(-0.5), data.size() - T(0.5), probs);
+
+  int left = std::max(int(std::floor(poi)), int(0));
+  int right = std::min(int(std::ceil(poi)), int(data.size() - 1));
+
+  if (probs <= T(0.5))
+    std::nth_element(data.begin(), data.begin() + left, data.end());
+  else
+    std::nth_element(data.begin(), data.begin() + right, data.end());
+
+  T datLeft = data[left];
+  T datRight = data[right];
+
+  T quantile = Lerp(datLeft, datRight, poi - T(left));
+
+  return quantile;
+}
+
+// akima interpolation
+/*
+Ref. : Hiroshi Akima, Journal of the ACM, Vol. 17, No. 4, October 1970,
+      pages 589-602.
+*/
+template <typename T>
+std::vector<T>
+akimaInterp1(std::vector<T> const &x, std::vector<T> const &y, std::vector<T> const &xi, bool save_Mode = true)
+{
+  // check inputs
+
+  //calculate u vector
+  auto uVec = [](std::vector<T> const &x, std::vector<T> const &y) {
+    size_t n = x.size();
+    std::vector<T> u((n + 3));
+    for (size_t i = 1; i < n; ++i)
+    {
+      u[i + 1] = (y[i] - y[i - 1]) / (x[i] - x[i - 1]); // Shift i to i+2
+    }
+
+    auto akima_end = [](const T &u1, const T &u2) {
+      return 2.0 * u1 - u2;
+    };
+
+    u[1] = akima_end(u[2], u[3]);
+    u[0] = akima_end(u[1], u[2]);
+    u[n + 1] = akima_end(u[n], u[n - 1]);
+    u[n + 2] = akima_end(u[n + 1], u[n]);
+
+    return u;
+  };
+  std::vector<T> u = uVec(x, y);
+
+  // calculate yp vector
+  std::vector<T> yp(x.size());
+  for (size_t i = 0; i < x.size(); ++i)
+  {
+    auto a = std::abs(u[i + 3] - u[i + 2]);
+    auto b = std::abs(u[i + 1] - u[i]);
+    if ((a + b) != 0)
+    {
+      yp[i] = (a * u[i + 1] + b * u[i + 2]) / (a + b);
+    }
+    else
+    {
+      yp[i] = (u[i + 2] + u[i + 1]) / 2.0;
+    }
+  }
+
+  // calculte interpolated yi values
+  auto kFind = [](const T &xii, const std::vector<T> &x, int start, int end) {
+
+    int klo = start;
+    int khi = end;
+    // // Find subinterval by bisection
+    while (khi - klo > 1)
+    {
+      int k = (khi + klo) / 2;
+      x[k] > xii ? khi = k : klo = k;
+    }
+    return klo;
+  };
+
+  std::vector<T> yi(xi.size());
+  for (size_t i = 0; i < xi.size(); ++i)
+  {
+    // Find the right place in the table by means of a bisection.
+    int k = kFind(xi[i], x, int(0), x.size() - 1);
+
+    // Evaluate Akima polynomial
+    T b = x[k + 1] - x[k];
+    T a = xi[i] - x[k];
+    yi[i] = y[k] + yp[k] * a + (3.0 * u[k + 2] - 2.0 * yp[k] - yp[k + 1]) * a * a / b + (yp[k] + yp[k + 1] - 2.0 * u[k + 2]) * a * a * a / (b * b);
+
+    // Differentiate to find the second-order interpolant
+    //ypi[i] = yp[k] + (3.0*u[k+2] - 2.0*yp[k] - yp[k+1])*2*a/b + (yp[k] + yp[k+1] - 2.0*u[k+2])*3*a*a/(b*b);
+
+    // Differentiate to find the first-order interpolant
+    //yppi[i] = (3.0*u[k+2] - 2.0*yp[k] - yp[k+1])*2/b + (yp[k] + yp[k+1] - 2.0*u[k+2])*6*a/(b*b);
+  }
+  return yi;
+}
+
+
+
+
+}
+
+////
+
+
+template <typename T>
+void vector_print(const std::vector<T> &vec)
+{
+
+    std::cout << "[ ";
+    for (int i = 0; i < vec.size() - 1; i++)
+    {
+        std::cout << vec[i] << ", ";
+    }
+    std::cout << vec[vec.size() - 1] << " ]" << std::endl;
+}
+
+
+
+//template <typename T>
+//void matrix_print(const std::vector<std::vector<T>> &mat)
+//{
+
+//    std::cout << "[";
+//    std::cout << std::endl;
+//    for (int i = 0; i < mat.size(); i++)
+//    {
+//        std::cout << "  [ ";
+//        if (mat[i].size() > 0)
+//        {
+//            for (int j = 0; j < mat[i].size() - 1; j++)
+//            {
+//                std::cout << mat[i][j] << ", ";
+//            }
+//            std::cout << mat[i][mat[i].size() - 1];
+//        }
+
+//        std::cout << " ]" << std::endl;
+//    }
+//    std::cout << std::endl;
+//    std::cout << "]" << std::endl;
+//}
+
+
+
+
+// ----------
+
+
+
+
+// ------ confidence levels (copied from energies_example.cpp)
+
+
+template <typename T>
+std::vector<std::vector<T>>
+set2conf(std::vector<T> set_0, size_t windowSize, size_t samples, T confidencelevel)
+{
+
+  std::random_device rd;  //seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<> dis(T(0), T(1));
+
+  // propabiliy vector
+  std::vector<T> prob_0 = ooc_functions::linspace(T(1) / T(set_0.size()), T(1) - T(1) / T(set_0.size()), set_0.size());
+  std::sort(set_0.begin(), set_0.end());
+  //ooc_functions::dualPivotSort(set_0);
+
+  // compute probability matrix of set_0
+  std::vector<std::vector<T>> m_0(samples, std::vector<T>(set_0.size()));
+  //std::vector<std::vector<T>> m_prop_0(samples, std::vector<T>(set_0.size()));
+
+  for (size_t i = 0; i < samples; ++i)
+  {
+    for (size_t j = 0; j < set_0.size(); ++j)
+    {
+      m_0[i][j] = T(dis(gen)); // fill with random numbers
+    }
+
+    std::sort(m_0[i].begin(), m_0[i].end()); // sort the row
+    //ooc_functions::dualPivotSort(m_prop_0[i]);
+    m_0[i] = ooc_functions::akimaInterp1(prob_0, set_0, m_0[i]); // interpolate the random numbers
+  }
+
+  // m_prop_0.clear();
+  // m_prop_0.shrink_to_fit();
+
+  // transpose
+  auto m_0t = ooc_functions::transpose(m_0);
+
+  m_0.clear();
+  m_0.shrink_to_fit();
+
+  // compute left and right confidence boundaries of set_0
+  std::vector<T> set_0_left(set_0.size());
+  std::vector<T> set_0_right(set_0.size());
+  for (size_t i = 0; i < set_0.size(); ++i)
+  {
+    set_0_left[i] = ooc_functions::quickQuantil(m_0t[i], (T(1) - confidencelevel) / T(2));
+    set_0_right[i] = ooc_functions::quickQuantil(m_0t[i], confidencelevel + (T(1) - confidencelevel) / T(2));
+  }
+
+    m_0t.clear();
+  m_0t.shrink_to_fit();
+
+  // compute probability matrix of left and right and medians of set_0
+  std::vector<std::vector<T>> m_prop_1(samples, std::vector<T>(windowSize));
+
+
+  for (size_t i = 0; i < samples; ++i)
+  {
+    for (size_t j = 0; j < windowSize; ++j)
+    {
+      m_prop_1[i][j] = T(dis(gen)); // fill with random numbers
+    }
+
+    std::sort(m_prop_1[i].begin(), m_prop_1[i].end()); // sort the row
+  }
+
+  std::vector<std::vector<T>> quants(3, std::vector<T>(windowSize));
+
+
+  // left
+  std::vector<std::vector<T>> m(samples, std::vector<T>(windowSize));
+  for (size_t i = 0; i < samples; ++i)
+  {
+      m[i] = ooc_functions::akimaInterp1(prob_0, set_0_left, m_prop_1[i]); // interpolate the random numbers
+  }
+
+  // set_0_left.clear();
+  // set_0_left.shrink_to_fit();
+
+  auto mt = ooc_functions::transpose(m);
+
+  for (size_t i = 0; i < windowSize; ++i)
+  {
+    quants[0][i] = ooc_functions::quickQuantil(mt[i], (T(1.0) - confidencelevel) / T(2.0));
+  }
+
+  //right
+  for (size_t i = 0; i < samples; ++i)
+  {
+      m[i] = ooc_functions::akimaInterp1(prob_0, set_0_right, m_prop_1[i]);
+  }
+
+  // set_0_right.clear();
+  // set_0_right.shrink_to_fit();
+
+  mt = ooc_functions::transpose(m);
+
+  for (size_t i = 0; i < windowSize; ++i)
+  {
+    quants[2][i] = ooc_functions::quickQuantil(mt[i], confidencelevel + (T(1.0) - confidencelevel) / T(2.0));
+  }
+
+  //median
+  for (size_t i = 0; i < samples; ++i)
+  {
+      m[i] = ooc_functions::akimaInterp1(prob_0, set_0, m_prop_1[i]);
+  }
+
+  mt = ooc_functions::transpose(m);
+  // m.clear();
+  // m.shrink_to_fit();
+
+  // m_prop_1.clear();
+  // m_prop_1.shrink_to_fit();
+
+  for (size_t i = 0; i < windowSize; ++i)
+  {
+    quants[1][i] = ooc_functions::quickQuantil(mt[i], T(0.5));
+  }
+
+  return quants;
+}
+
+template <typename T>
+std::vector<std::vector<std::vector<T>>>
+set2multiconf(std::vector<T> set_0, std::vector<uint32_t> windowSizes, size_t samples, T confidencelevel)
+{
+  std::vector<std::vector<std::vector<T>>> multiquants;
+  for (size_t i = 0; i < windowSizes.size(); ++i)
+  {
+    multiquants.push_back(set2conf(set_0, windowSizes[i], samples, confidencelevel));
+  }
+
+  return multiquants;
+}
+
+
+
+
+
+
+
+
+
+// ------ main
+
+
+using T = double;
+
+
+int main() {
+
+    // setup ----
+    int w_grid_size = 30;
+    int h_grid_size = 30;
+    double start_learn_rate = 0.8;
+    double final_learn_rate = 0.2;
+    unsigned int iterations = 10000;
+    double initial_neighbour_size = 5;
+    double neigbour_range_decay = 2;
+    long long random_seed = 0;
+
+    size_t cluster_num = 5;
+
+    std::vector<uint32_t> windowSizes = {12, 24, 48, 96, 192, 384};
+    uint32_t samples = 1000;
+    double confidencelevel = 0.99;
+
+    auto ds = read_csv_num<T>("anomaly_detector_data_1/script/energies01_short.csv");
+    // -----------
+
+
+    size_t num_levels = ds[0].size();
+
+    std::uniform_real_distribution<T> distribution (-1, 1);
+    metric::Euclidean<double> distance;
+    metric::Grid4 graph (w_grid_size, h_grid_size);
+
+    metric::SOM<std::vector<T>, metric::Grid4> som (
+                graph,
+                distance,
+                start_learn_rate,
+                final_learn_rate,
+                iterations,
+                distribution,
+                initial_neighbour_size,
+                neigbour_range_decay,
+                random_seed
+                );
+
+    som.train(ds);
+
+
+    auto nodes_data = som.get_weights();
+
+    auto [assignments, exemplars, counts] = metric::kmeans(nodes_data, cluster_num, 1000);
+
+
+    std::vector<std::vector<std::vector<double>>> clustered_energies (
+        counts.size(),
+        std::vector<std::vector<double>>(num_levels)
+        );
+
+    for (auto record : ds)
+    {
+        // find cluster id for a record
+        auto bmu = som.BMU(record);
+        auto cluster_index = assignments[bmu];
+        for (int j = 0; j < num_levels; j++)
+        {
+            clustered_energies[cluster_index][j].push_back(record[j]);
+        }
+    }
+
+
+    for (auto cluster_data : clustered_energies) {
+
+        for (int ci = 0; ci < cluster_data.size(); ci++)
+        {
+            auto energy_subband_data = cluster_data[ci];
+
+            // returns quants for a single subbund
+            std::vector<std::vector<std::vector<T>>> multiquants = set2multiconf(energy_subband_data, windowSizes, samples, confidencelevel);
+
+            std::cout << "----" << std::endl;
+            for (auto window : multiquants)
+            {
+                std::cout << "conf_l: " << std::endl;
+                vector_print(window[0]);
+                std::cout << "conf_m: " << std::endl;
+                vector_print(window[1]);
+                std::cout << "conf_r: " << std::endl;
+                vector_print(window[2]);
+            }
+        }
+
+    }
+
+
+
+
+
+    std::cout << " - done" << std::endl;
+
+    return 0;
+}
