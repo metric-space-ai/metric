8193c5fd177e89350b62ba6c722ec3967ae59cec
maxfilippov <maxfilippov@aol.com>
2021-09-12 19:18:14 +0300

    added brief descriptions

--- modules/mapping/clustering_som_anomaly_detector.cpp
+++ modules/mapping/clustering_som_anomaly_detector.cpp
@@ -14,8 +14,6 @@
 #include <fstream>
 #include <sstream>
 
-//#include <string>
-
 
 
 
--- modules/mapping/clustering_som_anomaly_detector.hpp
+++ modules/mapping/clustering_som_anomaly_detector.hpp
@@ -16,15 +16,47 @@ Copyright (c) 2021 Panda Team
 namespace metric {
 
 
+
+/**
+ * Anomaly detector based on clustering and dictribution comparison with SOM pre-selection
+ */
 template <typename T>
 class ClusteringSomAnomalyDetector {
 
 
 public:
 
+    /**
+     * @brief ClusteringSomAnomalyDetector  - loads model from JSON
+     * @param filename
+     */
     ClusteringSomAnomalyDetector(const std::string & filename);
 
-    ClusteringSomAnomalyDetector(    // create by training using features
+    /**
+     * @brief ClusteringSomAnomalyDetector  - creates model using training dataset
+     * ****
+     * Training scenario:
+     * - SOM is trained
+     * - SOM nodes are clustered via kMediods in the original feature space
+     * (so SOM acts only as subset selector, its 2d manifold space is not used)
+     * - Each training dataset point gets mapped to the cluster its BMU belongs to.
+     * - CDFs and confidence windows for different sets of quantile values are computed using sample distribution for each cluster
+     * // TODO describe in details
+     * ****
+     * @param ds  - training dataset, multidimensional timeseries
+     * @param num_clusters  - desired number of clusters
+     * @param som_iterations  - number of iterations in SOM training
+     * @param som_initial_neighbour_size
+     * @param som_neigbour_range_decay
+     * @param som_w_grid_size
+     * @param som_h_grid_size
+     * @param som_start_learn_rate
+     * @param som_final_learn_rate
+     * @param som_random_seed
+     * @param samples  - number of points in reconstructed distributions used for empirical confidence level computation
+     * @param confidence_level
+     */
+    ClusteringSomAnomalyDetector(   // create by training using features
             const std::vector<std::vector<T>> & ds,
             const size_t num_clusters = 4, //10;
             const unsigned int som_iterations = 100, //10000,
@@ -40,10 +72,24 @@ public:
             const double confidence_level = 0.99
             );
 
+    /**
+     * @brief save  - write trained model to JSON
+     * @param filename
+     */
     void save(const std::string & filename);
 
+    /**
+     * @brief encode  - computes anomaly score for data of same format as used for training, per point, except 384 first points
+     * // TODO describe in details
+     * @param dataset  - data to derive anomaly score
+     * @param entropy_threshold  - rate of maximum entropy considered to trigger cluster fix
+     * @return  - vector of anomaly scores or -1 values in the points where cluster lock was not established
+     */
     std::vector<T> encode(const std::vector<std::vector<T>> & dataset, T entropy_threshold = 0.8);
 
+    /**
+     * @brief encode_debug  - same as encode, but with more output
+     */
     std::vector<std::vector<T>> encode_debug(const std::vector<std::vector<T>> & dataset, T entropy_threshold = 0.8);
 
 
