8ca0564258c0d3e98eff8fa94c7a0b3597037eb3
Kataev Victor <kataev.victor.1234@gmail.com>
2021-11-04 14:53:54 +0000

    remove debug print

--- tests/transform_test/dwt_tests.cpp
+++ tests/transform_test/dwt_tests.cpp
@@ -103,8 +103,6 @@ TEMPLATE_TEST_CASE("dwt_d", "[transform]", BV/*, SV*/)
         if (diff > maxdiff)
             maxdiff = diff;
     }
-    std::cout << "md: " << maxdiff << std::endl;
-    std::cout << std::numeric_limits<double>::epsilon() << std::endl;
     REQUIRE( maxdiff <= std::numeric_limits<double>::epsilon()*1e6 );
 }
 
@@ -142,14 +140,14 @@ TEST_CASE("Wavelet reconstruction")
 
 }
 
-TEST_CASE("1d")
-{
-  using V = blaze::DynamicVector<double>;
-
-  V data = {1, 2, 3, 4};
-  auto [cA, cD] = wavelet::dwt(data, 1);
-
-  std::cout << cA << std::endl;
-  std::cout << cD << std::endl;
-
-}
+//TEST_CASE("1d")
+//{
+//  using V = blaze::DynamicVector<double>;
+//
+//  V data = {1, 2, 3, 4};
+//  auto [cA, cD] = wavelet::dwt(data, 1);
+//
+//  std::cout << cA << std::endl;
+//  std::cout << cD << std::endl;
+//
+//}
