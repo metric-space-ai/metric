b965323ed9d89933abf5859d4116e79a36512cf7
Kataev Victor <kataev.victor.1234@gmail.com>
2021-11-11 13:48:10 +0000

    fix test

--- tests/transform_test/wavelet_tests.cpp
+++ tests/transform_test/wavelet_tests.cpp
@@ -12,7 +12,7 @@ TEMPLATE_TEST_CASE("DaubechiesMat", "[transform][wavelet]", float)
   for (size_t i = 0; i < dmat.rows() / 2; ++i) {
     REQUIRE(Approx(sumLow) == blaze::sum(blaze::row(dmat, i)));
   }
-  const auto sumHigh = blaze::sum(blaze::row(dmat, 0));
+  const auto sumHigh = blaze::sum(blaze::row(dmat, dmat.rows() / 2));
   for (size_t i = dmat.rows() / 2; i < dmat.rows(); ++i) {
     REQUIRE(Approx(sumHigh) == blaze::sum(blaze::row(dmat, i)));
   }
