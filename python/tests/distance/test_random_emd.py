0347846035620b495e5d8f57839f77d1f1ca1902
Kataev Victor <kataev.victor.1234@gmail.com>
2021-11-22 22:00:48 +0000

    add remd operator test

--- python/tests/distance/test_random_emd.py
+++ python/tests/distance/test_random_emd.py
@@ -1,6 +1,11 @@
 from metric.distance import RandomEMD
-import numpy
+import numpy as np 
+import math
 
 
 def test__constructor():
+    d1 = np.array(range(11))
+    d2 = np.array(range(1, 12))
     remd = RandomEMD()
+
+    assert(math.isclose(remd(d1, d2), 1, 0.01)
