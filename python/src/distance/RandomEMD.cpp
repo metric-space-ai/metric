a982ab1c38d8b0f22c530f3f279c418269d2668a
Kataev Victor <kataev.victor.1234@gmail.com>
2021-11-22 16:27:17 +0000

    add operator() wrapper

--- python/src/distance/RandomEMD.cpp
+++ python/src/distance/RandomEMD.cpp
@@ -17,37 +17,21 @@
 namespace py = pybind11;
 
 
-template<typename Value>
+template<typename Sample, typename D = double>
 void wrap_metric_RandomEMD(py::module& m) {
-    using Container = std::vector<std::vector<Value>>;
-    using Vector = std::vector<Value>;
-    using Class = metric::RandomEMD<Value>;
 
+	using Class = metric::RandomEMD<Sample, D>;
     auto emd = py::class_<Class>(m, "RandomEMD", "Random Earth mover's distance");
     emd.def(py::init<>(), "Default constructor");
-    /*emd.def(py::init<Container&&>(), "Move constructor",
-        py::arg("cost_matrix")
-    );
-    emd.def(py::init<const Container&, const Value&>(),
-        py::arg("cost_matrix"),
-        py::arg("extra_mass_penalty") = -1
-    );
-    emd.def(py::init<std::size_t, std::size_t, const Value&>(),
-        py::arg("rows"),
-        py::arg("cols"),
-        py::arg("extra_mass_penalty") = -1
-    );
-    Value (Class::*call)(const Vector&, const Vector&) const = &Class::template operator()<Vector>;
+	emd.def(py::init<double>(),
+			py::arg("precision")
+	);
+	D (Class::*call)(const Sample&, const Sample&) const = &Class::template operator()<Sample>;
     emd.def("__call__", call);
-    Container (*func1)(size_t,size_t) = &metric::EMD_details::ground_distance_matrix_of_2dgrid<Value>;
-    //Container (*func2)(Container) = &metric::EMD_details::ground_distance_matrix_of_2dgrid<Value>;
-    emd.def_static("ground_distance_matrix_of_2dgrid", func1, py::arg("cols"), py::arg("rows"));
-    //emd.def("ground_distance_matrix_of_2dgrid", func2, (py::arg("grid"));
-    emd.def_static("max_in_distance_matrix", metric::EMD_details::max_in_distance_matrix<Container>);
-*/}
+}
 
 void export_metric_RandomEMD(py::module& m) {
-    wrap_metric_RandomEMD<double>(m);
+    wrap_metric_RandomEMD<std::vector<double>, double>(m);
 }
 
 
