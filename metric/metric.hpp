bd30f3b1e25a28a71c597a1108d817b766f8576d
Kataev Victor <kataev.victor.1234@gmail.com>
2022-02-09 20:14:20 +0000

    reformat code

--- metric/metric.hpp
+++ metric/metric.hpp
@@ -1,15 +1,15 @@
 /*
-                       
+
 PANDA presents
 
 ███╗   ███╗███████╗████████╗██████╗ ██╗ ██████╗
-████╗ ████║██╔════╝╚══██╔══╝██╔══██╗██║██╔════╝   
-██╔████╔██║█████╗     ██║   ██████╔╝██║██║       
-██║╚██╔╝██║██╔══╝     ██║   ██╔══██╗██║██║       
-██║ ╚═╝ ██║███████╗   ██║   ██║  ██║██║╚██████╗  
-                        Licensed under MPL 2.0. 
-                        Michael Welsch (c) 2018.
-                                                                                                   
+████╗ ████║██╔════╝╚══██╔══╝██╔══██╗██║██╔════╝
+██╔████╔██║█████╗     ██║   ██████╔╝██║██║
+██║╚██╔╝██║██╔══╝     ██║   ██╔══██╗██║██║
+██║ ╚═╝ ██║███████╗   ██║   ██║  ██║██║╚██████╗
+						Licensed under MPL 2.0.
+						Michael Welsch (c) 2018.
+
 A framework to process arbitrary data
 
 This Source Code Form is subject to the terms of the Mozilla Public
@@ -19,14 +19,13 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Copyright (c) 2018 Michael Welsch
 */
 
-
 #ifndef _PANDA_METRIC_HPP
 #define _PANDA_METRIC_HPP
 
-#include "distance.hpp"
-#include "space.hpp"
 #include "correlation.hpp"
-#include "transform.hpp"
+#include "distance.hpp"
 #include "mapping.hpp"
+#include "space.hpp"
+#include "transform.hpp"
 
 #endif
