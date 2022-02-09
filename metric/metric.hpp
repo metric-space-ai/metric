f92ed5e9cfba23cb3bf8ff71653670ef986c63df
Kataev Victor <kataev.victor.1234@gmail.com>
2022-02-09 19:23:03 +0000

    move metric.hpp to metric/

--- metric/metric.hpp
+++ metric/metric.hpp
@@ -10,7 +10,7 @@ PANDA presents
                         Licensed under MPL 2.0. 
                         Michael Welsch (c) 2018.
                                                                                                    
-a framework to process abritrary data
+A framework to process arbitrary data
 
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -20,18 +20,13 @@ Copyright (c) 2018 Michael Welsch
 */
 
 
-/*
-blaze need additional linking to blas.
-clang++ -std=c++17 main.cpp -o run.o -Ofast -L/usr/local/Cellar/openblas/0.3.5/lib -L/usr/local/Cellar/openblas/0.3.5/include -lopenblas
- */
-
 #ifndef _PANDA_METRIC_HPP
 #define _PANDA_METRIC_HPP
 
-#include "metric/distance.hpp"
-#include "metric/space.hpp"
-#include "metric/correlation.hpp"
-#include "metric/transform.hpp"
-#include "metric/mapping.hpp"
+#include "distance.hpp"
+#include "space.hpp"
+#include "correlation.hpp"
+#include "transform.hpp"
+#include "mapping.hpp"
 
 #endif
